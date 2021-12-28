a2cd0f5697ed23b84e94685a32e26c89cdce4d6a
Emanuele Galli <egalli64@gmail.com>
2021-12-28 13:13:51 +0100

    null

---
+++ m1/p1/d05/e3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Null pointer\n\n";
+
+	int* ptr = nullptr;
+	cout << "An unused pointer: " << ptr << endl;
+	cout << "A null pointer can't be dereferenced! " << *ptr << endl;
+}
+
