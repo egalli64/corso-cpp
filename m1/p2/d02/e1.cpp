5b0c703639560319e75a13bcbf986c01e0d46d4a
Emanuele <egalli64@gmail.com>
2021-12-28 19:15:29 +0100

    cleaner p2

--- m1/p2/d02/e1.cpp
+++ m1/p2/d02/e1.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "Hello!";
-    if(argc == 1)
-    	cout << " The caller didn't pass any parameter\n";
+    switch(argc) {
+    case 1:
+        cout << "The caller didn't pass any parameter\n";
+        break;
+    case 2:
+        cout << "The first parameter is " << argv[1] << endl;
+        break;
+    default:
+        cout << "The last parameter is " << argv[argc-1] << endl;
+        break;
+    }
 }
