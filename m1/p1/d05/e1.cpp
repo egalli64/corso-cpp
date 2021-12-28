1d35511ddc4d565e240bba35f3a551f26e6d2e3e
Emanuele Galli <egalli64@gmail.com>
2021-12-28 12:43:48 +0100

    array, ptr, ref

---
+++ m1/p1/d05/e1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "char* " << sizeof(char*) << endl;
+	cout << "double* " << sizeof(double*) << endl;
+	cout << endl;
+
+	char c = 'x';
+	char* pc = &c;
+
+	cout << c << ' ' << *pc << endl;
+	*pc = 'z';
+	cout << c << ' ' << *pc << endl;
+}
+
---
+++ m1/p1/d05/e2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int data[6] = { 42, 12, 18, 21, 5, 33 };
+	cout << "Element in last position: " << data[5] << endl;
+
+	int value = 84;
+	int* ptr = &value;
+	cout << "A single int variable, address and value: " << ptr << " -> " << *ptr << endl;
+
+	ptr = &data[0];
+	cout << "First cell in an array, address and value: " << ptr << " -> " << *ptr << endl;
+
+	ptr = data;
+	cout << "Same: " << ptr << " -> " << *ptr << endl;
+
+	ptr = &data[5];
+	cout << "Last cell: " << ptr << " -> " << *ptr << endl;
+
+	int& ref = value;
+	cout << "ref and value " << ref << ' ' << value << endl;
+
+	ref += 1;
+	cout << "ref and value after increasing ref " << ref << ' ' << value << endl;
+
+	int& ref2 = data[5];
+	cout << "ref2 and data[5] " << ref2 << ' ' << data[5] << endl;
+
+	ref2 += 1;
+	cout << "ref2 and data[5] after increasing ref2 " << ref2 << ' ' << data[5] << endl;
+}
+
