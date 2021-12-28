039846afe0934b547e56e0de3be6b9d5059e22bc
Emanuele Galli <egalli64@gmail.com>
2021-12-28 12:58:08 +0100

    constants

---
+++ m1/p1/d04/e4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\nConstants\n\n"; 
+	constexpr auto x = 18; // evaluated at compile time
+//	x += 1; // error!
+
+	const auto y = 42; // won't change
+//	y += 1; // error!
+
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+}
+
