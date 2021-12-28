41b1ec290b7c3e856ece8c242189f2153cffe8a6
Emanuele Galli <egalli64@gmail.com>
2021-12-28 11:26:33 +0100

    1-4

---
+++ m1/p1/d04/e1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "bool " << sizeof(bool) << endl;
+	cout << "char " << sizeof(char) << endl;
+	cout << "short " << sizeof(short) << endl;
+	cout << "int " << sizeof(int) << endl;
+	cout << "long " << sizeof(long) << endl;
+	cout << "long long " << sizeof(long long) << endl;
+	cout << endl;
+
+	cout << "float " << sizeof(float) << endl;
+	cout << "double " << sizeof(double) << endl;
+	cout << "long double " << sizeof(long double) << endl;
+	cout << endl;
+
+	bool b = true; // actually, 1
+	char c = 'x';
+	int i = 49'385; // optional digit separator C++14
+	double d = 3.14159'26535;
+	cout << b << ' ' << c << ' ' << i << ' ' << d << endl;
+
+	signed short int ssi = 0xFFFF; //  Hexadecimal notation
+	unsigned short int usi = 0xFFFF;
+	cout << "signed short integer: " << ssi << ", unsigned short int: " << usi << endl;
+
+	cout << "Implicit convertions, both widening and narrowing\n\n";
+	int ci = c;
+	bool bi = i;
+	cout << c << " -> " << ci << " ... " << i << " -> " << bi << endl;
+}
+
