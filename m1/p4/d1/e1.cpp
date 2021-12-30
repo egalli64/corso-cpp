e8659d8b79f52cf2a9d91c95b4a746c942e5210e
Emanuele <egalli64@gmail.com>
2021-12-31 00:02:48 +0100

    inheritance

---
+++ m1/p4/d1/e1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+private:
+    string name_;
+public:
+    Animal(string name) : name_(name) {}
+
+    const string& name() const { return name_; }
+};
+
+class Dog : public Animal
+{
+private:
+    string owner_;
+public:
+    Dog(string name, string owner) : Animal(name), owner_(owner) {}
+
+    const string& owner() const { return owner_; }
+};
+
+int main()
+{
+    Animal tom("Tom");
+    cout << tom.name() << endl;
+
+    Dog bob("Bob", "Robert Smith");
+    cout << bob.name() << " owned by " << bob.owner() << endl;
+}
