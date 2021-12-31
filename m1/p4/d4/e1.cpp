f56fb475c2c2a43d5033204b0ea1dde471dbe875
Emanuele <egalli64@gmail.com>
2021-12-31 10:21:51 +0100

    final

---
+++ m1/p4/d4/e1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief A final class
+ */
+class Animal final
+{
+private:
+    string name_;
+public:
+    Animal(const string& name) : name_(name) {}
+
+    const string& name() const { return name_; }
+};
+
+/**
+ * @brief can't extend a final class
+ */
+ // class Dog : public Animal {};
+
+int main()
+{
+    Animal tom("Tom");
+    cout << tom.name() << endl;
+}
---
+++ m1/p4/d4/e2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief An abstract class
+ */
+class Animal
+{
+private:
+    string name_;
+public:
+    Animal(const string& name) : name_(name) {}
+
+    const string& name() const { return name_; }
+    // pure virtual method
+    virtual string fullName() = 0;
+};
+
+class Dog : public Animal
+{
+private:
+    string owner_;
+public:
+    Dog(const string& name, const string& owner) : Animal(name), owner_(owner) {}
+
+    string fullName() override final
+    {
+        return Animal::name() + " owned by " + owner_;
+    }
+};
+
+class Poodle : public Dog
+{
+public:
+    Poodle(const string& name, const string& owner) : Dog(name, owner) {}
+
+    // can't override a final method
+    // string fullName() override { return "Poodle " + Dog::fullName(); }
+};
+
+int main()
+{
+    Dog bob("Bob", "Robert Smith");
+    cout << bob.fullName() << endl;
+
+    Poodle tom("Tom", "Thomas Benson");
+    cout << tom.fullName() << endl;
+}
