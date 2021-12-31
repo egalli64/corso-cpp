75c89c1b328de161a1795113c447d6c53a716e32
Emanuele <egalli64@gmail.com>
2021-12-31 10:05:24 +0100

    ABC

---
+++ m1/p4/d3/e1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief An abstract class
+ */
+class Animal
+{
+private:
+    string name_;
+public:
+    Animal(const string& name) : name_(name) {}
+
+    const string& name() const { return name_; }
+    // pure virtual method
+    virtual string fullName() = 0;
+};
+
+/**
+ * @brief to be concrete it must override the base class abstract methods
+ */
+class Dog : public Animal
+{
+private:
+    string owner_;
+public:
+    Dog(const string& name, const string& owner) : Animal(name), owner_(owner) {}
+
+    string fullName() override
+    {
+        return Animal::name() + " owned by " + owner_;
+    }
+};
+
+int main()
+{
+    // Can't instantiate an abstract class
+    // Animal animal("Tom");
+
+    Dog bob("Bob", "Robert Smith");
+    cout << bob.name() << endl;
+    cout << bob.fullName() << endl;
+
+    cout << "It is a polimorphic dog" << endl;
+    Animal* pa = &bob;
+    cout << pa->fullName() << endl;
+}
