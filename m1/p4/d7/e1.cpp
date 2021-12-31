cfa438e09da009a5f8bcc755e1bb5b6de322ac3a
Emanuele <egalli64@gmail.com>
2021-12-31 17:24:01 +0100

    protected/private inheritance

---
+++ m1/p4/d7/e1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Tail
+{
+private:
+    int count_;
+public:
+    Tail(int count) : count_(count) {}
+
+    void wag(const string& name) const
+    {
+        cout << name << ": ";
+        for (int i = 0; i < count_; i++)
+        {
+            cout << "wag";
+        }
+        cout << endl;
+    }
+};
+
+/**
+ * @brief Dog has-a Tail
+ */
+class Dog : private Tail
+{
+private:
+    string name_;
+public:
+    Dog(const string& name, int count) : Tail(count), name_(name) {}
+
+    void bark()
+    {
+        cout << name_ << ": bark!" << endl;
+        wag(name_);
+    }
+};
+
+int main()
+{
+    Dog tom("Tom", 3);
+    tom.bark();
+    // Tail methods are not part of the dog interface
+    // tom.wag();
+}
