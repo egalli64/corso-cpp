de15d2911d8ce0511e0f269b4a5e905d9b781c42
Emanuele <egalli64@gmail.com>
2022-01-01 23:45:37 +0100

    more vector

--- m1/p5/d2/e2.cpp
+++ m1/p5/d2/e2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <memory>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Entry
@@ -16,6 +17,11 @@ public:
 
     void setKey(int key) { key_ = key; };
     void setValue(const string& value) { value_ = value; }
+
+    virtual void sayHello() const
+    {
+        cout << "Hello " << value() << endl;
+    }
 };
 
 ostream& operator<<(ostream& os, const Entry& entry)
@@ -33,14 +39,49 @@ void print(const vector<Entry>& data)
     cout << endl;
 }
 
+class ExtendedEntry : public Entry
+{
+public:
+    ExtendedEntry(int key, const string& value) : Entry(key, value) {}
+    void sayHello() const override
+    {
+        cout << "Hello " << value() << "!!!" << endl;
+    }
+};
+
 int main()
 {
+    // OK, but only if we are _not_ interested in polymorphism 
     vector<Entry> empty;
     cout << "Size of an empty vector is " << empty.size() << endl;
 
-    vector<Entry> data{ {1, "hello"}, {2, "ciao"} };
+    vector<Entry> data{ {1, "Tom"}, {2, "Bob"} };
     print(data);
 
-    data.push_back({ 3, "hola" });
+    data.push_back({ 3, "Kim" });
     print(data);
+
+    ExtendedEntry wim{ 4, "Wim" };
+    wim.sayHello();
+
+    // BAD! Wim is sliced off !!!
+    data.push_back(wim);
+    for (const Entry& cur : data)
+    {
+        cur.sayHello();
+    }
+    cout << endl;
+
+    // polymorphic container 
+    vector<unique_ptr<Entry>> polyData;
+    polyData.push_back(make_unique<Entry>(1, "Tom"));
+
+    polyData.push_back(make_unique<ExtendedEntry>(2, "Bob"));
+    for (const auto& cur : polyData)
+    {
+        cur->sayHello();
+    }
+    cout << endl;
+
+    polyData[0]->sayHello();
 }
