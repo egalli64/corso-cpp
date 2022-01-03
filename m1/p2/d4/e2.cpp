27120318bf4849b1753209919cd6dca637bad03d
Emanuele <egalli64@gmail.com>
2022-01-03 23:37:23 +0100

    more code

--- m1/p2/d4/e2.cpp
+++ m1/p2/d4/e2.cpp
@@ -9,6 +9,14 @@ int dieCast() {
     return rand() % MAX_VALUE + 1;
 }
 
+double avg(int* values, int size) {
+    double result = 0;
+    for (int i = 0; i < size; i++) {
+        result += values[i];
+    }
+    return result / size;
+}
+
 int main() {
     srand(time(0));
 
@@ -16,18 +24,26 @@ int main() {
     int size;
     cin >> size;
 
-    int scores[MAX_VALUE * 2 + 1]{ 0 };
+    if (size < 1 || size > 100'000'000) {
+        cout << "Please, insert a reasonable value" << endl;
+        return 0;
+    }
+
+    int scores[MAX_VALUE * 2 + 1]{ };
     int* casts = new int[size];
     for (int i = 0; i < size; ++i) {
         casts[i] = dieCast() + dieCast();
         scores[casts[i]] += 1;
         cout << casts[i] << ' ';
     }
+    cout << "\n\nAverage: " << avg(casts, size) << endl;
 
-    cout << "\n\nDistribution" << endl;
+    // not used anymore
+    delete[] casts;
+
+    cout << "Distribution" << endl;
     for (int i = 2; i < MAX_VALUE * 2 + 1; ++i) {
         cout << '[' << i << ": " << static_cast<double>(scores[i]) / size << ']';
     }
     cout << endl;
-    delete[] casts;
 }
