c105798c874a8d2d1e208702b46a50c894b77b7b
Emanuele <egalli64@gmail.com>
2022-01-04 18:03:09 +0100

    static cast

--- m1/p2/d1/e3.cpp
+++ m1/p2/d1/e3.cpp
@@ -7,7 +7,7 @@ int main() {
 
     //	int y {42.7}; // error: explicit narrowing required
 
-    int y{ int(42.7) };
+    int y{ static_cast<int>(42.7) };
     auto z = 42.7; // automatic type deduction (here: double)
 
     cout << "x = " << x << endl;
