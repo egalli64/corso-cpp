e0ecb79715ca5fd734f9a4651362a9a2357b4255
Emanuele <egalli64@gmail.com>
2022-01-07 11:25:03 +0100

    vector review

--- m1/p7/d2/e1.cpp
+++ m1/p7/d2/e1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
-void print(const vector<int>& data) {
-    for (int cur : data) {
-        cout << cur << ' ';
+template<typename T>
+void print(const vector<T>& values, const string& label) {
+    cout << label << ": ";
+    for (const T& value : values) {
+        cout << value << ' ';
     }
     cout << endl;
 }
@@ -13,15 +16,24 @@ int main() {
     vector<int> empty;
     cout << "Size of an empty vector is " << empty.size() << endl;
 
-    vector<int> data{ 6, 4, 3 };
-    print(data);
+    vector<int> anotherEmpty{ 5 };
+    cout << "Size of another empty vector is " << empty.size() << endl;
 
-    // no filling in vector
+    vector<int> filled(5, -1);
+    print(filled, "A vector filled with a given value");
+
+    // no filling for vector
     // values.fill(-1);
 
+    vector<int> given{ 6, 4, 3 };
+    print(given, "A vector with a few given values");
+
+    vector<string> sv{ "hello" };
+    print(sv, "A string vector");
+
     // increase all the elements
-    for (int& cur : data) {
+    for (int& cur : given) {
         cur += 1;
     }
-    print(data);
+    print(given, "Increased values");
 }
