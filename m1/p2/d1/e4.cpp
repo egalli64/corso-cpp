e3f8bddb8316e90967bbd29aa3561d033dce36bd
Emanuele <egalli64@gmail.com>
2022-01-07 15:43:52 +0100

    enum class review

--- m1/p2/d1/e4.cpp
+++ m1/p2/d1/e4.cpp
@@ -15,12 +15,16 @@ int main() {
     cout << "y = " << y << endl;
 
     // simple enumeration
-    enum { ALPHA, BETA };
+    enum { ALPHA, BETA = 28 };
 
     cout << "ALPHA is " << ALPHA << " and BETA is " << BETA << endl;
 
     // improved enumeration - scoped and strongly typed
-    enum class GreekLetter { ALPHA, BETA };
+    enum class GreekLetter { ALPHA = 42, BETA };
     cout << "ALPHA is " << static_cast<int>(GreekLetter::ALPHA);
     cout << " and BETA is " << static_cast<int>(GreekLetter::BETA) << endl;
+
+    enum class WeekendDay : bool { SAT, SUN };
+    cout << "SAT is " << static_cast<int>(WeekendDay::SAT);
+    cout << " and SUN is " << static_cast<int>(WeekendDay::SUN) << endl;
 }
