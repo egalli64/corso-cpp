5078af1a18a092937593f54711cd98ce275969bb
Emanuele <egalli64@gmail.com>
2022-01-09 22:28:38 +0100

    for review

--- m1/p3/d4/e1.cpp
+++ m1/p3/d4/e1.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    cout << argv[0] << " has " << argc - 1 << " parameters" << endl;
-    for (int i = 1; i < argc; ++i) {
-        cout << i << ") " << argv[i] << endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << "Hello!" << endl;
     }
 }
--- m1/p3/d4/e2.cpp
+++ m1/p3/d4/e2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int data[] = { 1,2,3,4,5,6 };
-    for (int value : data) {
-        cout << value << endl;
+int main(int argc, char** argv) {
+    cout << argv[0] << " has " << argc - 1 << " parameters" << endl;
+    for (int i = 1; i < argc; ++i) {
+        cout << i << ") " << argv[i] << endl;
     }
 }
