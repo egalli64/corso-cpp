c5b84991362554620af4437944c079e4113c5277
Emanuele <egalli64@gmail.com>
2022-01-10 17:43:02 +0100

    Explicit use of move semantics

--- m2/p1/d1/e2.cpp
+++ m2/p1/d1/e2.cpp
@@ -6,7 +6,9 @@ class Base {
 private:
     const int x_;
 public:
-    Base(int x) : x_(x) {}
+    Base(int x) : x_{ x } {
+        cout << "Base ctor " << x_ << endl;
+    }
 
     virtual void hello() const {
         cout << "Base hello " << x_ << endl;
@@ -45,19 +47,30 @@ void unique() {
     up->hello();
 }
 
-unique_ptr<Base> uniqueMoved() {
-    cout << "\nReturning a unique smart pointer" << endl;
+unique_ptr<Base> getUnique() {
+    cout << "\nMoving a unique smart pointer out" << endl;
     unique_ptr<Base> up = make_unique<Derived>(3);
     up->hello();
 
     return up;
 }
 
+void giveUnique(unique_ptr<Base> up) {
+    cout << "\nA unique smart pointer is moved in" << endl;
+    up->hello();
+}
+
 int main() {
     raw();
     unique();
 
-    unique_ptr<Base> up3 = uniqueMoved();
+    unique_ptr<Base> up3 = getUnique();
+
+    giveUnique(move(up3));
+
+    if (!up3) {
+        cout << "\nAs expected, the unique smart pointer has no object associated anymore" << endl;
+    }
 
     cout << "Last statement in main" << endl;
 }
