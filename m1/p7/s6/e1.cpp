1a6c64c84633139d28c245418c969c9cced15544
Emanuele Galli <egalli64@gmail.com>
2022-02-16 18:27:38 +0100

    more examples

--- m1/p7/s6/e1.cpp
+++ m1/p7/s6/e1.cpp
@@ -12,5 +12,27 @@ void print(const map<string, double>& items) {
 
 int main() {
     map<string, double> data{ {"tom", 32.18}, {"bob", 44.11}, {"kim", 98.03} };
+    data.insert({"mia", 91.12});
+    data.emplace("jim", 12.23);
     print(data);
+
+    // -std=c++17
+    // if(auto it = data.find("bob"); it != data.end()) {
+    //     cout << "bob value is: " << it->second << endl;
+    // }
+
+    auto it = data.find("bob");
+    if (it != data.end()) {
+        cout << "find bob: " << it->second << endl;
+    }
+
+    cout << "at tom: " << data.at("tom") << endl;
+
+    try {
+        double joe = data.at("joe");
+        cout << joe << endl;
+    }
+    catch (const out_of_range& ex) {
+        cout << "Out of range calling " << ex.what() << " on joe" << endl;
+    }
 }
--- m1/p7/s6/e2.cpp
+++ m1/p7/s6/e2.cpp
@@ -12,5 +12,23 @@ void print(const unordered_map<string, double>& items) {
 
 int main() {
     unordered_map<string, double> data{ {"tom", 32.18}, {"bob", 44.11}, {"kim", 98.03} };
+    data.insert({"mia", 91.12});
+    data.emplace("jim", 12.23);
     print(data);
+    
+    auto it = data.find("bob");
+    if (it != data.end()) {
+        cout << "find bob: " << it->second << endl;
+    }
+
+    cout << "at tom: " << data.at("tom") << endl;
+
+    try {
+        double joe = data.at("joe");
+        cout << joe << endl;
+    }
+    catch (const out_of_range& ex) {
+        cout << "Out of range calling " << ex.what() << " on joe" << endl;
+    }
+
 }
