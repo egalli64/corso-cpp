5b8ffcb1eac082fcdf26669adcfe55f6f1cb44a2
Emanuele Galli <egalli64@gmail.com>
2022-04-18 11:41:59 +0200

    another placeholder example

--- m1/lambda/s3.cpp
+++ m1/lambda/s3.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void print(const string& message, const vector<int>& data) {
     cout << message << ": ";
     copy(data.cbegin(), data.cend(), ostream_iterator<int>(cout, " "));
-    cout << endl;
+    cout << '\n';
 }
 
 void increaseByOne(int& value) {
@@ -19,25 +19,29 @@ void increase(int& value, int delta) {
     value += delta;
 }
 
+void increaseEx(int delta, int& value) {
+    value += delta;
+}
+
 int main() {
     vector<int> values{ 1, 2, 3, 4, 5 };
     print("A few values", values);
 
-    cout << "Increase by 1 using for_each + function" << endl;
+    cout << "Increase by 1 using for_each + function\n";
     for (int i = 0; i < 3; ++i) {
         for_each(values.begin(), values.end(), increaseByOne);
         cout << i;
         print(" ...", values);
     }
 
-    cout << "Increase by [0 .. 3) using for_each + lambda, capturing by value" << endl;
+    cout << "Increase by [0 .. 3) using for_each + lambda, capturing by value\n";
     for (int i = 0; i < 3; ++i) {
         for_each(values.begin(), values.end(), [i](int& cur) { cur += i;});
         cout << i;
         print(" ...", values);
     }
 
-    cout << "Increase always by 0! The capture is done when the lambda is created" << endl;
+    cout << "Increase always by 0! The capture is done when the lambda is created\n";
     int i = 0;
     auto wrong = [i](int& cur) { cur += i;};
     while (i < 3) {
@@ -47,7 +51,7 @@ int main() {
         i += 1;
     }
 
-    cout << "Capturing by reference (be careful with this one!)" << endl;
+    cout << "Capturing by reference (be careful with this one!)\n";
     i = 0;
     auto risky = [&i](int& cur) { cur += i;};
     while (i < 3) {
@@ -57,9 +61,16 @@ int main() {
         i += 1;
     }
     
-    cout << "(Spoiler: using for_each + bind to increase by an adapted function)" << endl;
+    cout << "(Spoiler: using for_each + bind to increase by an adapted function)\n";
     for (int i = 0; i < 3; ++i) {
         for_each(values.begin(), values.end(), bind(increase, std::placeholders::_1, i));
+        cout << i;
+        print(" ...", values);
+    }
+
+    cout << "Placeholder in second position\n";
+    for (int i = 0; i < 3; ++i) {
+        for_each(values.begin(), values.end(), bind(increaseEx, i, std::placeholders::_1));
         cout << i;
         print(" ...", values);
     }
