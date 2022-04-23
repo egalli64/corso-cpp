26c5d21aa828bea358da6f13c14b720398a55b5e
Emanuele Galli <egalli64@gmail.com>
2022-04-23 10:50:43 +0200

    detach

---
+++ mt/e3.cpp
@@ -0,0 +1,19 @@
+/*
+ * g++ -pthread -o a.out e3.cpp
+ */
+#include <iostream>
+#include <thread>
+
+void a_function() {
+    std::cout << "Enter task\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds{100});
+    std::cout << "Task done!\n";
+}
+
+int main() {
+    std::thread t1{ a_function };
+    std::cout << "Do not wait for task\n";
+    t1.detach();
+
+    std::cout << "Main done!\n";
+}
