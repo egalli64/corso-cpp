62ec3806831620cc675b9e487a5533b6241b7f78
Emanuele Galli <egalli64@gmail.com>
2022-05-09 20:52:55 +0200

    another lock_guard example

---
+++ m2/concurrency/c_synchro/e6_guard.cpp
@@ -0,0 +1,26 @@
+/*
+ * g++ -pthread -o a.out e6_guard.cpp
+ */
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int main() {
+    int result = 0;
+    std::mutex mtx;
+
+    std::thread t{ [&result, &mtx] {
+        for (int i = 0; i < 50'000; ++i) {
+            std::lock_guard<std::mutex> lock{ mtx };
+            result++;
+        }
+    } };
+
+    for (int i = 0; i < 50'000; ++i) {
+        std::lock_guard<std::mutex> lock{ mtx };
+        result++;
+    }
+
+    t.join();
+    std::cout << "Guarded behavior: " << result << '\n';
+}
