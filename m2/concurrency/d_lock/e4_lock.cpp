a2f7040a37657f35148c018849b823767c8fa02d
Emanuele Galli <egalli64@gmail.com>
2022-05-11 22:05:44 +0200

    lock

--- m2/concurrency/d_lock/e4_lock.cpp
+++ m2/concurrency/d_lock/e4_lock.cpp
@@ -1,5 +1,5 @@
 /*
- * g++ -pthread -o a.out e3_deadlock.cpp
+ * g++ -pthread -o a.out e4_lock.cpp
  */
 #include <cmath>
 #include <cstdlib>
@@ -24,44 +24,40 @@ namespace {
 }
 
 int main() {
-    double v1 = 0;
-    double v2 = 0;
+    double v1 = 12.23;
+    double v2 = 32.21;
     std::mutex mtx_1;
     std::mutex mtx_2;
 
-    std::cout << "!!! This code could lead to a deadlock !!!\n";
-
     std::thread t1{ [&] {
         print("thread 1");
-
-        std::lock_guard<std::mutex> lock1{ mtx_1 };
-        print("lock 1");
 
         srand(time(nullptr));
         v1 = cbrt(log(rand() % 100 + 1));
         print("set v1", v1, v2);
 
-        std::lock_guard<std::mutex> lock2{ mtx_2 };
-        print("lock 2");
+        std::lock(mtx_1, mtx_2);
+        std::lock_guard<std::mutex> lock1{ mtx_1, std::adopt_lock };
+        std::lock_guard<std::mutex> lock2{ mtx_2, std::adopt_lock };
+        print("thread 1 locked");
 
         std::swap(v1, v2);
-        print("swap", v1, v2);
+        print("swap 1 ->", v1, v2);
     } };
 
     std::thread t2{ [&] {
         print("thread 2");
 
-        std::lock_guard<std::mutex> lock2{ mtx_2 };
-        print("lock 2");
-
         v2 = cbrt(42);
         print("set v2", v1, v2);
 
-        std::lock_guard<std::mutex> lock1{ mtx_1 };
-        print("lock 1");
+        std::lock(mtx_1, mtx_2);
+        std::lock_guard<std::mutex> lock2{ mtx_2, std::adopt_lock };
+        std::lock_guard<std::mutex> lock1{ mtx_1, std::adopt_lock };
+        print("thread 2 locked");
 
         std::swap(v1, v2);
-        print("swap", v1, v2);
+        print("swap 2 ->", v1, v2);
     } };
 
     t1.join();
