9cb6ac132f6d0ac3007e02ccf6f3ff1016ddfd4a
Emanuele Galli <egalli64@gmail.com>
2022-05-12 21:25:36 +0200

    raw shmem

--- m2/concurrency/f_shmem/e1_create_write.cpp
+++ m2/concurrency/f_shmem/e1_create_write.cpp
@@ -3,25 +3,24 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
-using namespace std;
 
 static const key_t SHM_KEY = 0xBAD1DEA;
 static int SHM_SEGMENT_SIZE = 1024;
 
 int main() {
-	cout << "Write 'hello' to shared memory" << endl;
+	std::cout << "Write 'hello' to shared memory\n";
 
 	// create a shared memory segment with a given key and size
 	int shmid = shmget(SHM_KEY, SHM_SEGMENT_SIZE, 0666 | IPC_CREAT);
 	if (shmid == -1) {
-		cerr << "Can't get shared memory" << endl;
+		std::cerr << "Can't get shared memory\n";
 		return 1;
 	}
 
 	// attach the shared memory segment to the address space, read/write
 	void* pTemp = shmat(shmid, nullptr, 0);
 	if (pTemp == (void*)-1) {
-		cerr << "Can't attach to shared memory" << endl;
+		std::cerr << "Can't attach to shared memory\n";
 		return 1;
 	}
 	else { // data transfer
--- m2/concurrency/f_shmem/e2_read_remove.cpp
+++ m2/concurrency/f_shmem/e2_read_remove.cpp
@@ -3,38 +3,37 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
-using namespace std;
 
 static const key_t SHM_KEY = 0xBAD1DEA;
 static int SHM_SEGMENT_SIZE = 1024;
 
 int main() {
-	cout << "Read from shared memory" << endl;
+	std::cout << "Read from shared memory\n";
 
 	// access a shared memory segment with a given key and size
 	int shmid = shmget(SHM_KEY, SHM_SEGMENT_SIZE, 0666);
 	if (shmid == -1) {
-		cerr << "Can't get shared memory" << endl;
+		std::cerr << "Can't get shared memory\n";
 		return 1;
 	}
 
 	// attach the shared memory segment to the address space, read only
 	void* attachedMemory = shmat(shmid, nullptr, SHM_RDONLY);
 	if (attachedMemory == (void*)-1) {
-		cerr << "Can't attach to shared memory" << endl;
+		std::cerr << "Can't attach to shared memory\n";
 		attachedMemory = nullptr;
 
 		return 1;
 	}
 	else { // access the data
 		char* pSharedSegment = static_cast<char*>(attachedMemory);
-		cout << pSharedSegment << endl;
+		std::cout << pSharedSegment << '\n';
 	}
 
 	if (shmdt(attachedMemory) == -1) {
-		cout << "Can't detach the shared memory segment" << endl;
+		std::cerr << "Can't detach the shared memory segment\n";
 	}
 	if (shmctl(shmid, IPC_RMID, nullptr) == -1) {
-		cout << "Can't remove the shared memory segment" << endl;
+		std::cerr << "Can't remove the shared memory segment\n";
 	}
 }
