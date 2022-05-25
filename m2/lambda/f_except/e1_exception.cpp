53e4e0978554df019f20966c05597afb5a7d0533
Emanuele Galli <egalli64@gmail.com>
2022-05-25 23:18:10 +0200

    another example

--- m2/lambda/f_except/e1_exception.cpp
+++ m2/lambda/f_except/e1_exception.cpp
@@ -3,16 +3,24 @@
 #include <iostream>
 
 int main() {
+    try {
+        [] { throw std::runtime_error{ "Hello!" }; }();
+    }
+    catch (std::runtime_error& ex) {
+        std::cout << ex.what() << '\n';
+    }
+
     auto mySqrt = [](int x) {
-        if(x < 0) {
-            throw std::runtime_error("Value should not be negative");
+        if (x < 0) {
+            throw std::runtime_error{ "Value should not be negative" };
         }
         return sqrt(x);
     };
     std::cout << "Result is: " << mySqrt(42) << '\n';
     try {
         std::cout << "Result is: " << mySqrt(-42) << '\n';
-    } catch( std::runtime_error& ex) {
-        std::cout << "runtime error, " << ex.what() << '\n';
+    }
+    catch (std::runtime_error& ex) {
+        std::cout << ex.what() << '\n';
     }
 }
