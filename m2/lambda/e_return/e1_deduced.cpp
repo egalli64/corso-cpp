475bc3dc57df716642f5d1f3921a38b246866578
Emanuele Galli <egalli64@gmail.com>
2022-05-25 19:46:11 +0200

    review

--- m2/lambda/e_return/e1_deduced.cpp
+++ m2/lambda/e_return/e1_deduced.cpp
@@ -9,16 +9,20 @@ int main() {
     auto v2 = [] {return 42;}();
     std::cout << "The returned int: " << v2 << ", size: " << sizeof(v2) << '\n';
 
-    auto v2a = [] {return std::numeric_limits<long int>::max();}();
+    auto v2a = [] {return 5'000'000'000;}();
     std::cout << "The returned long int: " << v2a << ", size: " << sizeof(v2a) << '\n';
 
 
-    auto v3 = [] {return std::numeric_limits<float>::max();}();
+    auto v3 = [] {return 3.6F;}();
     std::cout << "The returned float: " << v3 << ", size: " << sizeof(v3) << '\n';
 
     auto v3a = [] {return std::numeric_limits<double>::max();}();
     std::cout << "The returned double: " << v3a << ", size: " << sizeof(v3a) << '\n';
 
-    auto v3b = [] {return std::numeric_limits<long double>::max();}();
-    std::cout << "The returned long double: " << v3b << ", size: " << sizeof(v3b) << '\n';
+
+    auto v4 = [] {return 1e400L;}();
+    std::cout << "The returned long double: " << v4 << ", size: " << sizeof(v4) << '\n';
+
+    auto v4a = [] {return std::numeric_limits<long double>::max();}();
+    std::cout << "The returned long double: " << v4a << ", size: " << sizeof(v4a) << '\n';
 }
--- m2/lambda/e_return/e2_explicit.cpp
+++ m2/lambda/e_return/e2_explicit.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
-using namespace std;
+#include <limits>
 
 int main() {
     auto v1 = []() -> char {return 'x';}();
-    cout << "The returned char: " << v1 << ", size: " << sizeof(v1) << '\n';
+    std::cout << "The returned char: " << v1 << ", size: " << sizeof(v1) << '\n';
 
-    auto v1a = []() -> int {return 'x';}();
-    cout << "The returned int: " << v1a << ", size: " << sizeof(v1a) << '\n';
+    auto v2 = []() -> int {return 'x';}();
+    std::cout << "The returned int: " << v2 << ", size: " << sizeof(v2) << '\n';
 
     // overflow in conversion from double to (max) int
-    auto v2 = []() -> int {return 2e+300;}();
-    cout << "The returned int: " << v2 << ", size: " << sizeof(v2) << '\n';
+    auto v2a = []() -> int {return 2e300;}();
+    std::cout << "The returned int: " << v2a << ", size: " << sizeof(v2a) << '\n';
+
+    auto v3 = []() -> long double {return 42;}();
+    std::cout << "The returned long double: " << v3 << ", size: " << sizeof(v3) << '\n';
+
+    // !!! floating constant exceeds range of double -> inf
+    auto v3a = []() -> long double {return 1e400;}();
+    std::cout << "The returned long double: " << v3a << ", size: " << sizeof(v3a) << '\n';
 }
