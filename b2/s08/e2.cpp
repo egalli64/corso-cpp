4fbc9fcd934f5592f8e208ed37be9ce8a36f30de
Emanuele Galli <egalli64@gmail.com>
2023-07-06 12:32:38 +0200

    split for nullptr

---
+++ b2/s08/e2.cpp
@@ -0,0 +1,17 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * nullptr
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Null pointer\n\n";
+
+    int* ptr = nullptr;
+    cout << "An unused pointer: " << ptr << '\n';
+    cout << "A null pointer can't be dereferenced ... " << '\n';
+    // Segmentation fault
+    // cout << *ptr << '\n';
+}
