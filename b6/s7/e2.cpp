a4fccf16d70736c226b3758ec2d0394bd56cb794
Emanuele Galli <egalli64@gmail.com>
2023-07-09 16:23:51 +0200

    review

--- b6/s7/e2.cpp
+++ b6/s7/e2.cpp
@@ -1,51 +1,68 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * Simple hierarchy, no MI
+ */
 #include <iostream>
 using namespace std;
 
-class Actor {
+class Actor
+{
 private:
     string name_;
+
 public:
-    Actor(const string& name) : name_(name) {
+    Actor(const string &name) : name_(name)
+    {
         cout << "Actor ctor for " << name << '\n';
     }
     ~Actor() { cout << "Actor dtor for " << name_ << '\n'; }
 
-    const string& name() const { return name_; }
+    const string &name() const { return name_; }
 };
 
-class Animal : public Actor {
+class Animal : public Actor
+{
 public:
-    Animal(const string& name) : Actor(name) {
+    Animal(const string &name) : Actor(name)
+    {
         cout << "Animal ctor for " << name << '\n';
     }
     ~Animal() { cout << "Animal dtor" << '\n'; }
 };
 
-class Player : public Actor {
+class Player : public Actor
+{
 private:
     string game_;
+
 public:
-    Player(const string& name, const string& game) : Actor(name), game_(game) {
+    Player(const string &name, const string &game) : Actor(name), game_(game)
+    {
         cout << "Player ctor for " << game << '\n';
     }
     ~Player() { cout << "Player dtor for " << game_ << '\n'; }
 
-    const string& game() const { return game_; }
+    const string &game() const { return game_; }
 };
 
-class Dog : public Player {
+class Dog : public Player
+{
 public:
-    Dog(const string& name, const string& game) : Player(name, game) {
+    Dog(const string &name, const string &game) : Player(name, game)
+    {
         cout << "Dog ctor\n";
     }
 
-    void message() {
+    void message()
+    {
         cout << name() << " likes playing " << game() << '\n';
     }
 };
 
-int main() {
-    Dog tom("Tom", "chess");
+int main()
+{
+    Dog tom{"Tom", "chess"};
     tom.message();
     cout << " ... but Tom should be an animal, too!" << '\n';
 }
--- b6/s7/e3.cpp
+++ b6/s7/e3.cpp
@@ -1,54 +1,72 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * MI with DDD problem
+ */
 #include <iostream>
 using namespace std;
 
-class Actor {
+class Actor
+{
 private:
     string name_;
+
 public:
-    Actor(const string& name) : name_(name) {
+    Actor(const string &name) : name_(name)
+    {
         cout << "Actor ctor for " << name << '\n';
     }
     ~Actor() { cout << "Actor dtor for " << name_ << '\n'; }
 
-    const string& name() const { return name_; }
+    const string &name() const { return name_; }
 };
 
-class Animal : public Actor {
+class Animal : public Actor
+{
 public:
-    Animal(const string& name) : Actor(name) {
+    Animal(const string &name) : Actor(name)
+    {
         cout << "Animal ctor for " << name << '\n';
     }
     ~Animal() { cout << "Animal dtor" << '\n'; }
 };
 
-class Player : public Actor {
+class Player : public Actor
+{
 private:
     string game_;
+
 public:
-    Player(const string& name, const string& game) : Actor(name), game_(game) {
+    Player(const string &name, const string &game) : Actor(name), game_(game)
+    {
         cout << "Player ctor for " << game << '\n';
     }
     ~Player() { cout << "Player dtor for " << game_ << '\n'; }
 
-    const string& game() const { return game_; }
+    const string &game() const { return game_; }
 };
 
-class Dog : public Animal, Player {
+class Dog : public Animal, Player
+{
 public:
-    Dog(const string& name, const string& game) : Animal(name), Player(name, game) {
+    // name should be passed both to Animal and Player!
+    Dog(const string &name, const string &game) : Animal(name), Player(name, game)
+    {
         cout << "Dog ctor" << '\n';
     }
 
-    void message() {
+    void message()
+    {
         // ambiguous! name as animal or as player?
         // cout << name() << " likes playing " << game() << '\n';
 
-        // explicitly choosing the "right" name 
+        // explicitly choosing the "right" name
         cout << Player::name() << " likes playing " << game() << '\n';
     }
 };
 
-int main() {
-    Dog tom("Tom", "chess");
+int main()
+{
+    Dog tom{"Tom", "chess"};
     tom.message();
 }
