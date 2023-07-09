4813b7e93723377cc9f8e69a28999bf3257e2ab1
Emanuele Galli <egalli64@gmail.com>
2023-07-09 10:21:07 +0200

    review

---
+++ b6/s5/e3.cpp
@@ -0,0 +1,53 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * pure virtual / final method
+ */
+#include <iostream>
+using namespace std;
+
+// An abstract class
+class Animal
+{
+private:
+    string name_;
+
+public:
+    Animal(const string &name) : name_(name) {}
+
+    const string &name() const { return name_; }
+    // pure virtual method
+    virtual string fullName() = 0;
+};
+
+class Dog : public Animal
+{
+private:
+    string owner_;
+
+public:
+    Dog(const string &name, const string &owner) : Animal(name), owner_(owner) {}
+
+    string fullName() override final
+    {
+        return Animal::name() + " owned by " + owner_;
+    }
+};
+
+class Poodle : public Dog
+{
+public:
+    Poodle(const string &name, const string &owner) : Dog(name, owner) {}
+
+    // can't override a final method
+    // string fullName() override { return "Poodle " + Dog::fullName(); }
+};
+
+int main()
+{
+    Dog bob{"Bob", "Robert Smith"};
+    cout << bob.fullName() << '\n';
+
+    Poodle tom{"Tom", "Thomas Benson"};
+    cout << tom.fullName() << '\n';
+}
