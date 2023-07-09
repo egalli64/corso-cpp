9d5b4dc82093ac1814712bf3f1e2d1980bdff86f
Emanuele Galli <egalli64@gmail.com>
2023-07-09 16:23:51 +0200

    review

--- b6/s7/e1.cpp
+++ b6/s7/e1.cpp
@@ -1,43 +1,58 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * MI
+ */
 #include <iostream>
 using namespace std;
 
-class Animal {
+class Animal
+{
 private:
     string name_;
+
 public:
-    Animal(const string& name) : name_(name) {
+    Animal(const string &name) : name_(name)
+    {
         cout << "Animal ctor for " << name << '\n';
     }
     ~Animal() { cout << "Animal dtor for " << name_ << '\n'; }
 
-    const string& name() const { return name_; }
+    const string &name() const { return name_; }
 };
 
-class Player {
+class Player
+{
 private:
     string game_;
+
 public:
-    Player(const string& game) : game_(game) {
+    Player(const string &game) : game_(game)
+    {
         cout << "Player ctor for " << game << '\n';
     }
     ~Player() { cout << "Player dtor for " << game_ << '\n'; }
 
-    const string& game() const { return game_; }
+    const string &game() const { return game_; }
 };
 
-class Dog : public Animal, Player {
+class Dog : public Animal, Player
+{
 public:
-    Dog(const string& name, const string& game) : Animal(name), Player(game) {
+    Dog(const string &name, const string &game) : Animal(name), Player(game)
+    {
         cout << "Dog ctor" << '\n';
     }
     ~Dog() { cout << "Dog dtor" << '\n'; }
 
-    void message() {
+    void message()
+    {
         cout << name() << " likes playing " << game() << '\n';
     }
 };
 
-int main() {
-    Dog tom("Tom", "chess");
+int main()
+{
+    Dog tom{"Tom", "chess"};
     tom.message();
 }
