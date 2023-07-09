41998f8f3060c178cb76911e5a6fddaf2363c105
Emanuele Galli <egalli64@gmail.com>
2023-07-09 18:59:48 +0200

    review

--- b7/s6/e1.cpp
+++ b7/s6/e1.cpp
@@ -1,38 +1,50 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * SL map
+ */
 #include <iostream>
 #include <map>
 #include <string>
 using namespace std;
 
-void print(const map<string, double>& items) {
-    for (const auto& item : items) {
+void print(const map<string, double> &items)
+{
+    for (const auto &item : items)
+    {
         cout << item.first << ' ' << item.second << '\n';
     }
     cout << '\n';
 }
 
-int main() {
-    map<string, double> data{ {"tom", 32.18}, {"bob", 44.11}, {"kim", 98.03} };
+int main()
+{
+    map<string, double> data{{"tom", 32.18}, {"bob", 44.11}, {"kim", 98.03}};
     data.insert({"mia", 91.12});
     data.emplace("jim", 12.23);
     print(data);
 
     // -std=c++17
-    // if(auto it = data.find("bob"); it != data.end()) {
+    // if (auto it = data.find("bob"); it != data.end())
+    // {
     //     cout << "bob value is: " << it->second << '\n';
     // }
 
     auto it = data.find("bob");
-    if (it != data.end()) {
+    if (it != data.end())
+    {
         cout << "find bob: " << it->second << '\n';
     }
 
     cout << "at tom: " << data.at("tom") << '\n';
 
-    try {
+    try
+    {
         double joe = data.at("joe");
         cout << joe << '\n';
     }
-    catch (const out_of_range& ex) {
+    catch (const out_of_range &ex)
+    {
         cout << "Out of range calling " << ex.what() << " on joe" << '\n';
     }
 }
