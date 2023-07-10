df69d9f687241c3f23c39e528671832d0a8148af
Emanuele Galli <egalli64@gmail.com>
2023-07-10 09:21:32 +0200

    review

--- b7/s9/e1.cpp
+++ b7/s9/e1.cpp
@@ -1,19 +1,27 @@
-#include<algorithm>
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * SL algorithm: sort
+ */
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-void print(const vector<string>& items, const string& label) {
+void print(const vector<string> &items, const string &label)
+{
     cout << label << " [ ";
-    for (const auto& item : items) {
+    for (const auto &item : items)
+    {
         cout << item << ' ';
     }
     cout << "]\n";
 }
 
-int main() {
-    vector<string> data{ "tom", "bob", "kim" };
+int main()
+{
+    vector<string> data{"tom", "bob", "kim"};
     print(data, "A string vector");
     sort(data.begin(), data.end());
     stable_sort(data.begin(), data.end());
