29cb4c1d510e8a16fb9519cd35ca90a91ac8ff80
Emanuele Galli <egalli64@gmail.com>
2023-07-12 10:51:49 +0200

    review

---
+++ b10_MT/5_lock/e2_guard_int.cpp
@@ -0,0 +1,34 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * Lock guard on int
+ *
+ * g++ -pthread -o a.out e2_guard_int.cpp
+ */
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+int main()
+{
+    int result{0};
+    std::mutex mtx;
+    constexpr int count{50'000};
+
+    std::thread t{[&result, &mtx] {
+        for (int i = 0; i < count; ++i)
+        {
+            std::lock_guard<std::mutex> lock{mtx};
+            result++;
+        }
+    }};
+
+    for (int i = 0; i < count; ++i)
+    {
+        std::lock_guard<std::mutex> lock{mtx};
+        result++;
+    }
+
+    t.join();
+    std::cout << "Guarded behavior: " << result << '\n';
+}
---
+++ b10_MT/6_condition/e1.cpp
@@ -0,0 +1,54 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * unique_lock with defer_lock
+ *
+ * g++ -pthread -o a.out e1.cpp
+ */
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+namespace
+{
+std::mutex mtx;
+std::condition_variable cnd;
+
+bool ready{false};
+int stock{10};
+
+void consumer()
+{
+    while (stock > 0)
+    {
+        std::unique_lock<std::mutex> lock{mtx};
+        cnd.wait(lock, [] { return ready; });
+
+        stock -= 2;
+        std::cout << "C:" << stock << ' ';
+        ready = false;
+
+        cnd.notify_one();
+    }
+}
+} // namespace
+
+int main()
+{
+    std::thread t{consumer};
+
+    while (stock > 0)
+    {
+        std::unique_lock<std::mutex> lock{mtx};
+
+        std::cout << "P:" << ++stock << ' ';
+        ready = true;
+
+        cnd.notify_one();
+        cnd.wait(lock, [] { return !ready; });
+    }
+
+    t.join();
+    std::cout << '\n';
+}
