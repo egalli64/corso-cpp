38c244cea4d76d4a81a4b6455cfe7628b0de08fd
Emanuele Galli <egalli64@gmail.com>
2023-07-12 10:51:49 +0200

    review

---
+++ b10_MT/5_lock/e3_guard_vector.cpp
@@ -0,0 +1,53 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * Lock guard on vector
+ *
+ * g++ -pthread -o a.out e3_guard_vector.cpp
+ */
+#include <algorithm>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+namespace
+{
+std::vector<int> data{};
+std::mutex mtx;
+
+void add(int value)
+{
+    std::lock_guard<std::mutex> lock{mtx};
+    data.push_back(value);
+}
+
+bool contains(int value)
+{
+    std::lock_guard<std::mutex> lock{mtx};
+    return std::find(data.begin(), data.end(), value) != data.end();
+}
+} // namespace
+
+int main()
+{
+    if (contains(42))
+    {
+        std::cout << "Unexpected\n";
+    }
+
+    std::thread t{[] { add(42); }};
+
+    if (contains(42))
+    {
+        std::cout << "Could be, could be not\n";
+    }
+
+    t.join();
+    if (!contains(42))
+    {
+        std::cout << "Unexpected\n";
+    }
+
+    std::cout << "Done\n";
+}
