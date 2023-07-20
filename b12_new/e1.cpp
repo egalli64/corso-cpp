fd855f0595af01f082c2aa9eb16b651057e6b378
Emanuele <egalli64@gmail.com>
2023-07-20 19:37:03 +0200

    placement new

---
+++ b12_new/e1.cpp
@@ -0,0 +1,39 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * placement new
+ */
+#include <iostream>
+#include <new>
+
+class Simple
+{
+  private:
+    int id_;
+
+  public:
+    Simple(int id) : id_(id)
+    {
+        std::cout << "Ctor for " << id_ << '\n';
+    }
+
+    ~Simple()
+    {
+        std::cout << "Dtor for " << id_ << '\n';
+    }
+
+    int id()
+    {
+        return id_;
+    }
+};
+
+int main()
+{
+    constexpr size_t sz = sizeof(Simple);
+    char buffer[sz];
+
+    Simple *simple = new (buffer) Simple(42);
+    std::cout << "Simple id is " << simple->id() << '\n';
+    simple->~Simple();
+}
