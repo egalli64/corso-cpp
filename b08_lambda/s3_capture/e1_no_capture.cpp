931f0c3cf750bf1c797ab3c735f90a81b10a6193
Emanuele <egalli64@gmail.com>
2023-07-21 10:12:12 +0200

    std::copy

--- b08_lambda/s3_capture/e1_no_capture.cpp
+++ b08_lambda/s3_capture/e1_no_capture.cpp
@@ -13,7 +13,7 @@ namespace
 void print(const std::vector<int> &data, const std::string &message = "")
 {
     std::cout << message << ": ";
-    copy(data.cbegin(), data.cend(), std::ostream_iterator<int>(std::cout, " "));
+    std::copy(data.cbegin(), data.cend(), std::ostream_iterator<int>(std::cout, " "));
     std::cout << '\n';
 }
 
