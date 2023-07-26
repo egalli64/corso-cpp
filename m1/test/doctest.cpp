85383ad7b22961cf9b4db371ee8fca941fb805b1
Emanuele Galli <egalli64@gmail.com>
2023-07-26 16:50:12 +0200

    minor cleanup

--- m1/test/doctest.cpp
+++ m1/test/doctest.cpp
@@ -1,25 +1,34 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * test
+ */
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-#include <climits>
-#include <iostream>
 #include "buggy_factorial.h"
 #include "buggy_negator.h"
+#include <climits>
+#include <iostream>
 
-TEST_CASE("check vs require") {
+TEST_CASE("check vs require")
+{
     CHECK(0 == 1);
     REQUIRE(0 == 1);
     CHECK(0 == 2);
 }
 
-TEST_CASE("subcase") {
-    std::cout << "Root level" << std::endl;
+TEST_CASE("subcase")
+{
+    std::cout << "Root level\n";
 
-    SUBCASE("subcase 1") {
-        std::cout << "Subcase 1" << std::endl;
+    SUBCASE("subcase 1")
+    {
+        std::cout << "Subcase 1\n";
     }
 
-    SUBCASE("subcase 2") {
-        std::cout << "Subcase 2" << std::endl;
+    SUBCASE("subcase 2")
+    {
+        std::cout << "Subcase 2\n";
     }
 }
--- m1/test/test_negator.cpp
+++ m1/test/test_negator.cpp
@@ -1,11 +1,17 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * test
+ */
 #include "doctest.h"
 
-#include <climits>
 #include "buggy_negator.h"
+#include <climits>
 
-TEST_CASE("negator") {
+TEST_CASE("negator")
+{
     CHECK(negator(0) == 0);
     CHECK(negator(42) == -42);
     CHECK(negator(-63) == 63);
-    CHECK(negator(INT_MIN) == INT_MAX);
+    CHECK(negator(INT_MIN) == INT_MIN);
 }
