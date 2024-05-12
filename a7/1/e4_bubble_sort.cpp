6dd895d09fcbf6176d803ec4e58e0f8d96d638e4
Emanuele Galli <egalli64@gmail.com>
2024-05-12 19:26:55 +0200

    plain bubble sort

---
+++ a7/1/e4_bubble_sort.cpp
@@ -0,0 +1,41 @@
+/*
+ * Corso C++ a7 - https://github.com/egalli64/corso-cpp/a7
+ *
+ * Bubble sort - basic algorithm
+ */
+#include <iostream>
+
+void print(int *data, size_t size)
+{
+    std::cout << "[ ";
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << data[i] << ' ';
+    }
+    std::cout << "]\n";
+}
+
+int main()
+{
+    int values[]{6, 1, 3, 4};
+    constexpr size_t sz = sizeof(values) / sizeof(int);
+
+    std::cout << "Working on: ";
+    print(values, sz);
+
+    for (int i = 0; i < sz - 1; i++)
+    {
+        for (int j = 0; j < sz - 1; j++)
+        {
+            if (values[j] > values[j + 1])
+            {
+                int temp = values[j];
+                values[j] = values[j + 1];
+                values[j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Sorted: ";
+    print(values, sz);
+}
