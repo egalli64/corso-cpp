161732b7e7dd1cbcb66d3cc7ce60425ca10b6ffb
Emanuele Galli <egalli64@gmail.com>
2024-05-12 19:56:59 +0200

    Bubble sort (3) swap check

---
+++ a7/1/e4_bubble_sort_3.cpp
@@ -0,0 +1,56 @@
+/*
+ * Corso C++ a7 - https://github.com/egalli64/corso-cpp/a7
+ *
+ * Bubble sort (3) - stop looping when no swap detected
+ */
+#include <iostream>
+
+void print(int *data, size_t size)
+{
+    std::cout << "[ ";
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << data[i] << ' ';
+    }
+    std::cout << "]\n";
+}
+
+int main()
+{
+    // a random example
+    // int values[]{6, 1, 3, 4, 5, 2};
+
+    // check it on an almost-sorted array
+    int values[]{6, 1, 2, 3, 4, 5};
+    constexpr size_t sz = sizeof(values) / sizeof(int);
+
+    std::cout << "Working on: ";
+    print(values, sz);
+
+    for (int i = 0; i < sz - 1; i++)
+    {
+        std::cout << "External loop " << i << '\n';
+        bool swapped = false;
+        for (int j = 0; j < sz - i - 1; j++)
+        {
+            std::cout << "Internal loop " << j << '\n';
+            if (values[j] > values[j + 1])
+            {
+                std::cout << "Swap!\n";
+                swapped = true;
+
+                int temp = values[j];
+                values[j] = values[j + 1];
+                values[j + 1] = temp;
+            }
+        }
+
+        if (!swapped)
+        {
+            break;
+        }
+    }
+
+    std::cout << "Sorted: ";
+    print(values, sz);
+}
