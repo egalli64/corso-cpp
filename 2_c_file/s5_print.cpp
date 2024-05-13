be4b17cdf4b63478637ef4e8a25d9d5dfe322e6e
Emanuele Galli <egalli64@gmail.com>
2024-05-13 21:55:04 +0200

    fprintf / printf

---
+++ 2_c_file/s5_print.cpp
@@ -0,0 +1,24 @@
+/*
+ * Corso C++ - https://github.com/egalli64/corso-cpp
+ *
+ * stdio - fprintf / printf
+ */
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    const char *filename = "s5.txt";
+
+    // notice that "w" would overwrite an existing file!
+    FILE *fw = fopen(filename, "w");
+
+    int x = 42;
+
+    fprintf(fw, "Writing an integer (%d), a floating point number (%.10f), ", x, M_PI);
+    fprintf(fw, "a character (%c), and a string (%s)\n", x, filename);
+    fclose(fw);
+
+    printf("Check the result in %s\n", filename);
+}
