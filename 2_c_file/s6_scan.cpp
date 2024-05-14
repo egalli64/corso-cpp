c8809f9cf4ac9a4a74896f88f106c8c3bc645afd
Emanuele Galli <egalli64@gmail.com>
2024-05-14 09:40:59 +0200

    scan

---
+++ 2_c_file/s6_scan.cpp
@@ -0,0 +1,34 @@
+/*
+ * Corso C++ - https://github.com/egalli64/corso-cpp
+ *
+ * stdio - fscanf / scanf
+ */
+#include <cmath>
+#include <cstdio>
+
+int main()
+{
+    const char *filename = "s6.txt";
+    FILE *fr = fopen(filename, "r");
+
+    if (fr == NULL)
+    {
+        printf("Can't open file %s in read mode\n", filename);
+        return EXIT_FAILURE;
+    }
+
+    int x;
+    float pi;
+    double e;
+    char c;
+    char info[21];
+
+    fscanf(fr, "%d %f %lf %c %s", &x, &pi, &e, &c, info);
+    fclose(fr);
+
+    printf("The integer is %d\n", x);
+    printf("The float is %f\n", pi);
+    printf("The double is %f\n", e);
+    printf("The char is %c\n", c);
+    printf("The string is \"%s\"\n", info);
+}
