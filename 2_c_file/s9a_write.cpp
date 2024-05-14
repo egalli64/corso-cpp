266ff94fd53380f7e4693ac8effabd46443e8b3d
Emanuele Galli <egalli64@gmail.com>
2024-05-14 15:12:29 +0200

    clarify

--- 2_c_file/s9a_write.cpp
+++ 2_c_file/s9a_write.cpp
@@ -8,12 +8,12 @@
 int main()
 {
     int data[]{4343, 8723, 7012, 4855, 1203, 1985, 3424};
-    int sz = sizeof(data) / sizeof(int);
+    size_t sz = sizeof(data) / sizeof(int);
 
     const char *filename = "s9.bin";
-    FILE *fw = fopen(filename, "w");
+    FILE *fw = fopen(filename, "wb");
     int count = fwrite(data, sizeof(int), sz, fw);
     fclose(fw);
 
-    printf("%d values have been stored in the %s file\n", count, filename);
+    printf("%d values have been stored in the %s binary file\n", count, filename);
 }
