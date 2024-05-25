c41d03196b1c4b383acff702e7229a7497c157dd
Emanuele Galli <egalli64@gmail.com>
2024-05-25 11:47:05 +0200

    clarify

--- 04_function/s6_template/e5.cpp
+++ 04_function/s6_template/e5.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-template <typename T> T max(const T& a, const T& b)
+template <typename T> T max(const T &a, const T &b)
 {
     return a > b ? a : b;
 }
@@ -10,8 +10,12 @@ int main()
     int a = max(5, 10);
     std::cout << "Max is: " << a << '\n';
 
+    // won't compile, no implicit conversion from char to int
     // int a2 = max(5, '1');
-    // std::cout << "Max is: " << a << '\n';
+
+    // when required, use an explicit conversion
+    int a2 = max(5, static_cast<int>('1'));
+    std::cout << "Max is: " << a2 << '\n';
 
     float b = max(3.14f, 2.71f);
     std::cout << "Max is: " << b << '\n';
