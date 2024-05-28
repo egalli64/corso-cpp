f777a8f7108e57f9fef71746e81b6f83c64d83f5
Emanuele Galli <egalli64@gmail.com>
2024-05-28 20:27:38 +0200

    another example

--- 07_SL/s8_algorithm/e1.cpp
+++ 07_SL/s8_algorithm/e1.cpp
@@ -10,14 +10,17 @@
 
 int main()
 {
-    std::vector<int> values{7, 2, 12};
+    std::vector<int> values{7, 2, 12, 54, 34};
 
     // find in an interval a value
-    int target{2};
+    int target = 24;
     auto itTwo = find(values.begin(), values.end(), target);
     std::cout << target << (itTwo == values.end() ? " not " : " ") << "found\n";
 
     // accumulate (adding) in an interval starting from value zero
     int sum = accumulate(values.begin(), values.end(), 0);
     std::cout << "Sum of values is " << sum << '\n';
+
+    sum = accumulate(++values.begin(), --values.end(), 0);
+    std::cout << "Sum of internal values is " << sum << '\n';
 }
