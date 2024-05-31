fe4fd766d655626bc8f04a4699f34c420d3d3f31
Emanuele Galli <egalli64@gmail.com>
2024-05-31 19:31:03 +0200

    simplify, clarify

--- 04_function/s5_overload/e1.cpp
+++ 04_function/s5_overload/e1.cpp
@@ -5,6 +5,8 @@
  */
 #include <iostream>
 
+namespace
+{
 void swap(int &left, int &right)
 {
     std::cout << "swapping integers\n";
@@ -20,18 +22,20 @@ void swap(double &left, double &right)
     left = right;
     right = temp;
 }
+} // namespace
 
 int main()
 {
-    int a{42};
-    int b{18};
+    int a = 42;
+    int b = 18;
 
     std::cout << "a is " << a << " b is " << b << '\n';
     swap(a, b);
     std::cout << "a is " << a << " b is " << b << '\n';
+    std::cout << "---\n";
 
-    double d{42.82};
-    double e{18.09};
+    double d = 42.82;
+    double e = 18.09;
 
     std::cout << "d is " << d << " e is " << e << '\n';
     swap(d, e);
