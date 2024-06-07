8c343dd4fe6118b2df10d8acae6be5362a0b8e12
Emanuele Galli <egalli64@gmail.com>
2024-06-07 22:13:43 +0200

    readability

--- 08_lambda/s1_callable/e3_lambda.cpp
+++ 08_lambda/s1_callable/e3_lambda.cpp
@@ -10,11 +10,15 @@
 
 int main()
 {
-    auto print = [](const std::string &s) { std::cout << "-> " << s << '\n'; };
+    // given a prefix provided elsewhere
+    std::string prefix{"-> "};
+    std::vector<std::string> names{"Tom", "Bob", "Kim"};
 
+    // create a lambda that captures the prefix
+    auto print = [prefix](const std::string &s) -> void { std::cout << prefix << s << '\n'; };
     print("A lambda is a callable");
 
-    std::vector<std::string> names{"Tom", "Bob", "Kim"};
     std::cout << "Passing a lambda to a function:\n";
+    // pass the lambda to a HOF that uses it
     std::for_each(names.begin(), names.end(), print);
 }
