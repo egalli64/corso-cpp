956d4f91d6e27b0f2590e861be8139905ef90d4b
Emanuele Galli <egalli64@gmail.com>
2024-06-09 21:21:44 +0200

    robustness

--- 08_lambda/s3_capture/e2_by_value.cpp
+++ 08_lambda/s3_capture/e2_by_value.cpp
@@ -10,24 +10,24 @@
 
 namespace
 {
-void print(const std::vector<int> &data, const std::string &message = "")
+template <typename IT> void print(IT first, IT last, const std::string &message = "")
 {
     std::cout << message << ": ";
-    std::copy(data.cbegin(), data.cend(), std::ostream_iterator<int>(std::cout, " "));
+    std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
     std::cout << '\n';
 }
 } // namespace
 
 int main()
 {
-    std::vector<int> values{1, 2, 3};
-    print(values, "A few values");
+    std::vector<int> values{1, 2, 3, 4, 5, 6};
+    print(values.cbegin(), values.cend(), "A few values");
 
     std::cout << "Increase by [0 .. 3) using for_each + lambda, capturing by value\n";
     for (int i = 0; i < 3; ++i)
     {
         for_each(values.begin(), values.end(), [i](int &cur) { cur += i; });
-        print(values);
+        print(values.cbegin(), values.cend());
     }
 
     std::cout << "Increase always by 0! The capture is done when the lambda is created\n";
@@ -37,7 +37,7 @@ int main()
     while (i < 3)
     {
         for_each(values.begin(), values.end(), wrong);
-        print(values);
+        print(values.cbegin(), values.cend());
         i += 1;
     }
 }
