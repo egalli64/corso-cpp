1b9ff9354575c6eee3a266516386bbd13922c930
Emanuele Galli <egalli64@gmail.com>
2024-06-14 22:22:57 +0200

    [ ]

---
+++ 07_SL/x1_string/e4_operators.cpp
@@ -0,0 +1,27 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * std::string - operators
+ */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string alpha{"alpha"};
+
+    std::cout << alpha << ":\n";
+    std::cout << "First char is: " << alpha[0] << '\n';
+    std::cout << "Bound checking is our responsibility: " << alpha[42] << '\n';
+
+    try
+    {
+        std::cout << "Use at() when in doubt ... ";
+        std::cout << alpha.at(42) << '\n';
+    }
+    catch (const std::out_of_range &ex)
+    {
+        std::cout << "and try/catch its use\n";
+        std::cout << "Out of range exception: " << ex.what() << '\n';
+    }
+}
