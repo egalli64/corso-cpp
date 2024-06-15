26c8bf04c848342b208b93f4447f1c3ac2bade42
Emanuele Galli <egalli64@gmail.com>
2024-06-15 21:45:06 +0200

    size, [], and iterators

---
+++ 07_SL/s2_vector/e2_iterators.cpp
@@ -0,0 +1,47 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * SL vector - size, [], and iterators
+ */
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> values{6, 4, 3};
+
+    std::cout << "classic for loop on a vector (uncommon): ";
+    for (std::vector<int>::size_type i = 0; i < values.size(); ++i)
+    {
+        std::cout << values[i] << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "loop by const iterator: ";
+    for (auto it = values.cbegin(); it != values.cend(); ++it)
+    {
+        std::cout << *it << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "loop-increase by iterator: ";
+    for (auto it = values.begin(); it != values.end(); ++it)
+    {
+        std::cout << ++(*it) << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "range-for by value: ";
+    for (int cur : values)
+    {
+        std::cout << cur << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "range-for by const ref (not useful on int): ";
+    for (const int &cur : values)
+    {
+        std::cout << cur << ' ';
+    }
+    std::cout << '\n';
+}
