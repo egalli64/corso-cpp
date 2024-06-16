a919955d44ff2e84e790bc185fea9b496bc72d9f
Emanuele Galli <egalli64@gmail.com>
2024-06-16 09:11:01 +0200

    review

--- 07_SL/s2_vector/e8_poly_shared.cpp
+++ 07_SL/s2_vector/e8_poly_shared.cpp
@@ -1,7 +1,7 @@
 /*
  * Corso C++ https://github.com/egalli64/corso-cpp
  *
- * SL vector - achieving polymorphism by smart pointers (shared)
+ * achieving polymorphism by shared smart pointers
  */
 #include <iostream>
 #include <memory>
@@ -12,23 +12,25 @@
 
 int main()
 {
-    std::cout << "*** Shared Smart Pointer Vector\n";
+    std::cout << "*** Initializing the container with a list of share ptrs\n";
     std::vector<std::shared_ptr<Entry>> values{std::make_shared<Entry>(1, "Tom"), std::make_shared<Entry>(2, "Bob")};
+    std::cout << "*** Pushing shared ptrs on the container\n";
     values.push_back(std::make_shared<Entry>(3, "Kim"));
     values.push_back(std::make_shared<ExtendedEntry>(4, "Wim", "!!!"));
 
-    std::cout << "*** All entries say hello\n";
+    std::cout << "*** All entries say hello (by ref)\n";
     for (const auto &cur : values)
     {
         std::cout << "Use count is " << cur.use_count() << " ... ";
         cur->sayHello();
     }
 
+    std::cout << "*** All entries say hello (by copy)\n";
     for (auto cur : values)
     {
         std::cout << "Use count is " << cur.use_count() << " ... ";
         cur->sayHello();
     }
 
-    std::cout << "*** Vector is about to going out of scope\n";
+    std::cout << "*** The container is about to go out of scope\n";
 }
