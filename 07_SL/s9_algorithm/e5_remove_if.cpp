cffe4af83cbbab447fcdb5144d32ffa21ab1a1a0
Emanuele Galli <egalli64@gmail.com>
2024-06-19 09:22:24 +0200

    remove_if

---
+++ 07_SL/s9_algorithm/e5_remove_if.cpp
@@ -0,0 +1,40 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * SL algorithm: remove_if - erase
+ */
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace
+{
+template <typename IT> void print(IT cur, IT end, const std::string &label)
+{
+    std::cout << label << ": [ ";
+    for (; cur != end; ++cur)
+    {
+        std::cout << *cur << ' ';
+    }
+    std::cout << "]\n";
+}
+
+bool to_be_removed(int x)
+{
+    return x > 20;
+}
+} // namespace
+
+int main()
+{
+    std::vector<int> values{7, 23, 11, 23, 12};
+    print(values.cbegin(), values.cend(), "A vector");
+
+    auto end = std::remove_if(values.begin(), values.end(), to_be_removed);
+    print(values.cbegin(), values.cend(), "After removing selected values");
+    print(end, values.end(), "The tail to be erased");
+
+    values.erase(end, values.end());
+    print(values.cbegin(), values.cend(), "After erasing");
+}
