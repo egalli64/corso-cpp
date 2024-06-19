97d9a4531868f7d5c0b2a98a13d69ce352288196
Emanuele Galli <egalli64@gmail.com>
2024-06-19 12:04:41 +0200

    review

--- 09_memory/s3_unique/e5_move.cpp
+++ 09_memory/s3_unique/e5_move.cpp
@@ -7,39 +7,59 @@
 #include <iostream>
 #include <memory>
 
-std::unique_ptr<Thing> getUnique()
+namespace
 {
-    std::unique_ptr<Thing> up = std::make_unique<ThingExt>(1);
-    std::cout << "Returning a unique_ptr: ";
+/**
+ * @brief make a ThingExt unique_ptr by id
+ *
+ * @param id the Thing id
+ * @return the generated ThingExt as a Thing unique ptr
+ */
+std::unique_ptr<Thing> make_unique_thing(int id)
+{
+    std::cout << "*** make_unique_thing() for " << id << '\n';
+    std::unique_ptr<Thing> up = std::make_unique<ThingExt>(id);
     up->hello();
+
+    std::cout << "*** Returning a unique_ptr\n";
 
     // In modern C++ move and copy elision are automatically done by the compiler
     // return std::move(up);
-
     return up;
 }
 
-void passUnique(std::unique_ptr<Thing> up)
+/**
+ * @brief Print and reset the unique ptr passed in
+ *
+ * @param up a unique ptr - should be moved
+ */
+void pass_unique(std::unique_ptr<Thing> up)
 {
-    std::cout << "A unique_ptr has moved in: ";
-    up->hello();
+    std::cout << "*** A unique_ptr has moved in pass_unique(): " << *up << '\n';
 
-    up.reset(new ThingExt{2});
-    std::cout << "Reset for unique_ptr: ";
-    up->hello();
+    up.reset(new ThingExt{3});
+    std::cout << "*** After reset: " << *up << '\n';
 }
+} // namespace
 
 int main()
 {
-    std::unique_ptr<Thing> up = getUnique();
+    // the object received by copy elision is passed to the unique_ptr ctor
+    std::unique_ptr<Thing> up = make_unique_thing(1);
+    std::cout << "*** up is " << *up << '\n';
+
+    std::unique_ptr<Thing> up2;
+    // the object received by copy elision is passed to the unique_ptr assignment operator
+    up2 = make_unique_thing(2);
+    std::cout << "*** up2 is " << *up2 << '\n';
 
     // won't compile
-    // passUnique(up);
+    // pass_unique(up);
 
-    passUnique(std::move(up));
+    pass_unique(std::move(up));
 
     if (!up)
     {
-        std::cout << "As expected, the unique smart pointer has no object associated anymore\n";
+        std::cout << "*** As expected, the moved unique smart pointer has no object associated anymore\n";
     }
 }
