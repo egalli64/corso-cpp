ea7a5ab2e2be172d8f9edf3fea695fbab2e7b2dc
Emanuele Galli <egalli64@gmail.com>
2024-06-23 15:56:54 +0200

    more info

--- 04_function/s3_return/e2_by_reference.cpp
+++ 04_function/s3_return/e2_by_reference.cpp
@@ -9,9 +9,28 @@ namespace
 {
 int hidden_value = 42;
 
+/**
+ * @brief make available an hidden value in read-only mode
+ *
+ * @return the hidden value
+ */
 const int &answer()
 {
     return hidden_value;
+}
+
+/**
+ * @brief Returning a reference to a local variable is wrong!
+ *
+ * @return a reference to a local variable
+ */
+const int &mistake()
+{
+    int bad = 42;
+
+    std::cout << "This is a local variable: " << bad << '\n';
+
+    return bad;
 }
 } // namespace
 
@@ -19,15 +38,19 @@ int main()
 {
     const int &a = answer();
 
-    // won't compile
-    // x += 1;
+    // won't compile, a is a const ref
+    // a += 1;
     std::cout << "The answer a is " << a << '\n';
 
-    int b = answer();
-    b += 1;
-    std::cout << "The answer b is " << b << '\n';
+    const int &b = mistake();
+    // std::cout << "Undefined behavior: " << b << '\n';
+
+    // assign a const ref to a modifiable variable by copy
+    int c = answer();
+    c += 1;
+    std::cout << "The answer c is " << c << '\n';
     std::cout << "The hidden value is " << hidden_value << '\n';
 
-    // won't compile
-    // int& c = answer();
+    // won't compile, const is missing
+    // int& d = answer();
 }
