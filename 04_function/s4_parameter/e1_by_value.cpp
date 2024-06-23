d37e57600a0a3a9a19f30cc7d777d1425a92abf5
Emanuele Galli <egalli64@gmail.com>
2024-06-23 16:21:24 +0200

    more info

--- 04_function/s4_parameter/e1_by_value.cpp
+++ 04_function/s4_parameter/e1_by_value.cpp
@@ -7,7 +7,13 @@
 
 namespace
 {
-void wontSwap(int left, int right)
+/**
+ * @brief if the parameters are passed by values, no effect in their changes is seen in the caller
+ *
+ * @param left a parameter
+ * @param right another parameter
+ */
+void wont_swap(int left, int right)
 {
     std::cout << "Called by value: left is " << left << " and right is " << right << '\n';
 
@@ -26,6 +32,6 @@ int main()
 
     std::cout << "Caller by value: a is " << a << " and b is " << b << '\n';
 
-    wontSwap(a, b);
+    wont_swap(a, b);
     std::cout << "Caller by value: a is " << a << " and b is " << b << '\n';
 }
