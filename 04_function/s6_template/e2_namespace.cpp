f4d3663f0222d44941a7276fdf518d383a9690a8
Emanuele Galli <egalli64@gmail.com>
2024-06-24 10:19:18 +0200

    more info

--- 04_function/s6_template/e2_namespace.cpp
+++ 04_function/s6_template/e2_namespace.cpp
@@ -5,14 +5,21 @@
  */
 #include <iostream>
 #include <string>
+#include <utility>
 
 namespace my
 {
+/**
+ * @brief my template swap function
+ *
+ * @param left to be swapped
+ * @param right to be swapped
+ */
 template <typename T> void swap(T &left, T &right)
 {
-    T temp = left;
-    left = right;
-    right = temp;
+    T temp = std::move(left);
+    left = std::move(right);
+    right = std::move(temp);
 }
 } // namespace my
 
@@ -21,13 +28,19 @@ int main()
     int a = 42;
     int b = 18;
 
+    std::cout << "My swap on int\n";
     std::cout << "a is " << a << " b is " << b << '\n';
     my::swap(a, b);
-    std::cout << "a is " << a << " b is " << b << '\n';
+    std::cout << "a is " << a << " b is " << b << "\n\n";
+
+    std::cout << "Standard swap on int\n";
+    std::swap(a, b);
+    std::cout << "a is " << a << " b is " << b << "\n\n";
 
     std::string s = "hello";
     std::string t = "ciao";
 
+    std::cout << "My swap on string\n";
     std::cout << "s is " << s << " t is " << t << '\n';
     my::swap(s, t);
     std::cout << "s is " << s << " t is " << t << '\n';
