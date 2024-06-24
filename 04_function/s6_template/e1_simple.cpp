6f1663404c10d6901734d10ee8091b13e3f31d93
Emanuele Galli <egalli64@gmail.com>
2024-06-24 10:01:48 +0200

    more info

--- 04_function/s6_template/e1_simple.cpp
+++ 04_function/s6_template/e1_simple.cpp
@@ -6,10 +6,18 @@
 #include <iostream>
 #include <string>
 
+namespace
+{
+/**
+ * @brief a simple printing template function
+ * 
+ * @param x the T value to be printed
+ */
 template <typename T> void print(const T &x)
 {
     std::cout << "The value is " << x << '\n';
 }
+} // namespace
 
 int main()
 {
