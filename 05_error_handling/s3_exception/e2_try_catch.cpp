8dd5fb0225b045780dfe48596252ece6c77f5384
Emanuele Galli <egalli64@gmail.com>
2024-06-24 17:13:58 +0200

    more info

--- 05_error_handling/s3_exception/e2_try_catch.cpp
+++ 05_error_handling/s3_exception/e2_try_catch.cpp
@@ -7,6 +7,13 @@
 
 namespace
 {
+/**
+ * @brief a function that could throw an exception
+ *
+ * @param value it should not be 1
+ * @throw 42 when value is 1
+ * @note throwing something that is not a std::exception is not considered a good idea
+ */
 void risky(int value)
 {
     if (value == 1)
