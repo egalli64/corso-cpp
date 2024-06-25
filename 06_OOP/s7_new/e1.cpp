0309f7be7cea52bd36a5e43640afe602874b25d6
Emanuele Galli <egalli64@gmail.com>
2024-06-25 22:28:09 +0200

    more info

--- 06_OOP/s7_new/e1.cpp
+++ 06_OOP/s7_new/e1.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <memory>
 
+/**
+ * @brief A simple class
+ */
 class Player
 {
   private:
@@ -13,23 +16,41 @@ class Player
     double score_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Player(int id, double score) : id_(id), score_(score)
     {
     }
+
+    /**
+     * @brief dtor
+     */
     ~Player()
     {
         std::cout << "dtor " << id_ << ' ' << score_ << '\n';
     }
 
+    /**
+     * @brief id getter
+     */
     int id()
     {
         return id_;
     }
+
+    /**
+     * @brief score getter
+     */
     double score()
     {
         return score_;
     }
-    void increaseScore(double delta)
+
+    /**
+     * @brief
+     */
+    void increase_score(double delta)
     {
         score_ += delta;
     }
@@ -37,16 +58,19 @@ class Player
 
 int main()
 {
+    // object on stack
     Player p1{1, 102.12};
-    p1.increaseScore(12.21);
+    p1.increase_score(12.21);
     std::cout << p1.id() << ' ' << p1.score() << '\n';
 
+    // object on heap, ptr on stack
     Player *p2 = new Player(2, 203.45);
-    p2->increaseScore(23.3);
+    p2->increase_score(23.3);
     std::cout << p2->id() << ' ' << p2->score() << '\n';
     delete p2;
 
+    // object on heap, smart ptr on stack
     std::unique_ptr<Player> p3 = std::make_unique<Player>(3, 45.3);
-    p3->increaseScore(23.3);
+    p3->increase_score(23.3);
     std::cout << p3->id() << ' ' << p3->score() << '\n';
 }
