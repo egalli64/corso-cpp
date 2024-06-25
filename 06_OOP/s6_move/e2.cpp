8f4d9cce68ebeb34a619cddaccdb89eab72dcc78
Emanuele Galli <egalli64@gmail.com>
2024-06-25 20:32:46 +0200

    more info

--- 06_OOP/s6_move/e2.cpp
+++ 06_OOP/s6_move/e2.cpp
@@ -6,14 +6,20 @@
 #include <iostream>
 #include <string>
 
+/**
+ * A class to show different costs in creation/assignment
+ */
 class Player
 {
   private:
-    static const int SZ = 5;
+    static constexpr int SZ = 5;
     int id_;
     int *hand_;
 
   public:
+    /**
+     * @brief ctor is expensive, the full job has to be done
+     */
     explicit Player(int id) : id_(id)
     {
         hand_ = new int[SZ];
@@ -24,12 +30,9 @@ class Player
         std::cout << "Ctor (costly) " << id_ << '\n';
     }
 
-    ~Player()
-    {
-        delete[] hand_;
-        std::cout << "Dtor " << id_ << '\n';
-    }
-
+    /**
+     * @brief copy ctor is as expensive as the plain ctor
+     */
     Player(const Player &other)
     {
         id_ = other.id_;
@@ -42,6 +45,9 @@ class Player
         std::cout << "Copy ctor (costly) " << id_ << '\n';
     }
 
+    /**
+     * @brief assignment is cheaper, just reset the memory
+     */
     Player &operator=(const Player &other)
     {
         id_ = other.id_;
@@ -56,6 +62,9 @@ class Player
         return *this;
     }
 
+    /**
+     * @brief move ctor is very cheap
+     */
     Player(Player &&other)
     {
         id_ = other.id_;
@@ -66,6 +75,9 @@ class Player
         std::cout << "Move ctor (cheap!): " << id_ << '\n';
     }
 
+    /**
+     * @brief move assignment is very cheap
+     */
     Player &operator=(Player &&other)
     {
         id_ = other.id_;
@@ -80,6 +92,18 @@ class Player
         return *this;
     }
 
+    /**
+     * @brief dtor
+     */
+    ~Player()
+    {
+        delete[] hand_;
+        std::cout << "Dtor for player " << id_ << '\n';
+    }
+
+    /**
+     * @brief print id and associated values
+     */
     void print() const
     {
         std::cout << id_ << ": [ ";
@@ -91,7 +115,10 @@ class Player
     }
 };
 
-void swapCopy(Player &left, Player &right)
+/**
+ * @brief swap players by copy - could be cheaper
+ */
+void swap_copy(Player &left, Player &right)
 {
     std::cout << "Swap by copy\n";
 
@@ -100,7 +127,10 @@ void swapCopy(Player &left, Player &right)
     right = buffer;
 }
 
-void swapMove(Player &left, Player &right)
+/**
+ * @brief swap players by move - cheap!
+ */
+void swap_move(Player &left, Player &right)
 {
     std::cout << "Swap by move\n";
 
@@ -117,11 +147,11 @@ int main()
     Player p2{2};
     p2.print();
 
-    swapCopy(p1, p2);
+    swap_copy(p1, p2);
     p1.print();
     p2.print();
 
-    swapMove(p1, p2);
+    swap_move(p1, p2);
     p1.print();
     p2.print();
 }
