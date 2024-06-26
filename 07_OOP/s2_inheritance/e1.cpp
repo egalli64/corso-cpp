420280557743aa36bb45cee2c5b36dea6ca45509
Emanuele Galli <egalli64@gmail.com>
2024-06-26 21:05:15 +0200

    more info

--- 07_OOP/s2_inheritance/e1.cpp
+++ 07_OOP/s2_inheritance/e1.cpp
@@ -5,47 +5,79 @@
  */
 #include <iostream>
 
+/**
+ * @brief a simple base class
+ */
 class Animal
 {
   private:
     const std::string name_;
 
   public:
+    /**
+     * @brief default ctor
+     */
     Animal() : name_("Unkwnown"){};
+
+    /**
+     * @brief ctor
+     */
     Animal(const std::string &name) : name_(name)
     {
     }
 
+    /**
+     * @brief name getter
+     */
     const std::string &name() const
     {
         return name_;
     }
 };
 
+/**
+ * @brief put-to overload for Animal to ostream
+ */
 std::ostream &operator<<(std::ostream &os, const Animal &animal)
 {
     return os << animal.name();
 }
 
+/**
+ * @brief Dog IS-A Animal
+ */
 class Dog : public Animal
 {
   private:
     const std::string owner_;
 
   public:
+    /**
+     * @brief default ctor, implicitly call Animal default ctor
+     */
     Dog() : owner_("none")
     {
     }
+
+    /**
+     * @brief ctor, explicitly call Animal ctor
+     */
     Dog(const std::string &name, const std::string &owner) : Animal(name), owner_(owner)
     {
     }
 
+    /**
+     * @brief owner getter
+     */
     const std::string &owner() const
     {
         return owner_;
     }
 };
 
+/**
+ * @brief put-to overload for Dog to ostream
+ */
 std::ostream &operator<<(std::ostream &os, Dog dog)
 {
     return os << dog.name() << " owned by " << dog.owner();
