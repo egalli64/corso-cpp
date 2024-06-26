a415b613473adda26f0d6d133ad423ea80882a0d
Emanuele Galli <egalli64@gmail.com>
2024-06-26 14:18:39 +0200

    more info

--- 06_OOP/s8_modifiers/e3_static.cpp
+++ 06_OOP/s8_modifiers/e3_static.cpp
@@ -5,63 +5,87 @@
  */
 #include <iostream>
 
+/**
+ * @brief a simple class
+ */
 class Player
 {
   private:
+    /**
+     * @brief class field
+     */
     static int count_;
     int id_;
     double score_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Player(int id, double score) : id_(id), score_(score)
     {
+        // to stress that it is a class field could be referred as Player::count_
         count_ += 1;
         std::cout << "Ctor, count is " << count_ << '\n';
     }
 
+    /**
+     * @brief dtor
+     */
     ~Player()
     {
         count_ -= 1;
         std::cout << "Dtor, count is " << count_ << '\n';
     }
 
+    /**
+     * @brief id getter
+     */
     int id() const
     {
         return id_;
     }
+
+    /**
+     * @brief score getter
+     */
     double score() const
     {
         return score_;
     }
-    void increaseScore(double delta)
-    {
-        score_ += delta;
-    }
 
-    static void printCount();
+    /**
+     * @brief static method, just declared here
+     */
+    static void print_count();
 };
 
+// initialize the static data field
 int Player::count_ = 0;
 
-void Player::printCount()
+void Player::print_count()
 {
     std::cout << "Current number of players: " << count_ << '\n';
 }
 
+/**
+ * @brief a free function calling a public static method defined in an accessible class
+ */
 void another_function()
 {
     Player bob{12, 3.7};
-    Player::printCount();
+    // no need to go through a player object
+    Player::print_count();
 }
 
 int main()
 {
-    Player::printCount();
+    Player::print_count();
 
     Player tom{42, 33.12};
     // legal but a bit confusing: is it an instance method?
-    tom.printCount();
+    tom.print_count();
 
     another_function();
-    Player::printCount();
+    Player::print_count();
 }
