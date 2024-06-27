eeb3210cfc558d2e6c0ef7a6740011504e5d0faa
Emanuele Galli <egalli64@gmail.com>
2024-06-27 15:32:30 +0200

    more info

--- 07_OOP/s4_polymorphism/e2_slicing.cpp
+++ 07_OOP/s4_polymorphism/e2_slicing.cpp
@@ -5,55 +5,95 @@
  */
 #include <iostream>
 
+/**
+ * @brief a base class
+ */
 class Animal
 {
   private:
     std::string name_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Animal(const std::string &name) : name_(name)
     {
     }
 
+    /**
+     * @brief meant to be overwritten
+     */
     virtual std::string name() const
     {
         return name_;
     }
+
+    /**
+     * @brief virtual dtor
+     */
+    ~Animal()
+    {
+    }
 };
 
+/**
+ * @brief PolimorphicDog IS-An Animal, polimorphic in name()
+ */
 class PolimorphicDog : public Animal
 {
   private:
     std::string owner_;
 
   public:
+    /**
+     * @brief ctor
+     */
     PolimorphicDog(const std::string &name, const std::string &owner) : Animal(name), owner_(owner)
     {
     }
 
+    /**
+     * @brief gives a different meaning to name() for dogs
+     */
     std::string name() const override
     {
         return Animal::name() + " owned by " + owner_;
     }
 };
 
-void badIdea(Animal byValue)
+/**
+ * @brief passing an object by value is rarely a good idea
+ * @param a the Animal - expect slicing here!
+ */
+void bad_idea(Animal a)
 {
-    std::cout << "Animal passed by value (slicing!): " << byValue.name() << '\n';
+    std::cout << "Animal passed by value (slicing!): " << a.name() << '\n';
 }
 
-void normal(const Animal &byConstRef)
+/**
+ * @brief passing an object by const ref is the norm
+ * @param a the Animal - in the Liskov sense
+ */
+void normal(const Animal &a)
 {
-    std::cout << "Animal passed by const reference: " << byConstRef.name() << '\n';
+    std::cout << "Animal passed by const reference: " << a.name() << '\n';
 }
 
-void byPointer(const Animal *p)
+/**
+ * @brief passing an object by ptr is OK, but less common in modern C++
+ * @param a the Animal - in the Liskov sense
+ */
+void by_ptr(const Animal *a)
 {
-    if (p == nullptr)
+    if (a == nullptr)
     {
-        return;
+        std::cout << "Instead of an animal I've got a nullptr!\n";
     }
-    std::cout << "Animal passed by const ptr: " << p->name() << '\n';
+    else
+    {
+        std::cout << "Animal passed by const ptr: " << a->name() << '\n';
+    }
 }
 
 int main()
@@ -71,9 +111,9 @@ int main()
     PolimorphicDog bob{"Bob", "Robert Smith"};
     std::cout << "A polimorphic dog: " << bob.name() << '\n';
 
-    badIdea(bob);
+    bad_idea(bob);
     normal(bob);
-    byPointer(&bob);
+    by_ptr(&bob);
 
     std::cout << "An array of pointers is not affected by slicing" << '\n';
     Animal *animals[] = {&tom, &bob};
@@ -82,7 +122,7 @@ int main()
         std::cout << pa->name() << '\n';
     }
 
-    badIdea(*animals[1]);
+    bad_idea(*animals[1]);
     normal(*animals[1]);
-    byPointer(animals[1]);
+    by_ptr(animals[1]);
 }
