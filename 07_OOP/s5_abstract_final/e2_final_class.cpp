43254374cb557c9f7764121119736035394d9b19
Emanuele Galli <egalli64@gmail.com>
2024-06-27 16:06:15 +0200

    more info

--- 07_OOP/s5_abstract_final/e2_final_class.cpp
+++ 07_OOP/s5_abstract_final/e2_final_class.cpp
@@ -6,16 +6,25 @@
 #include <iostream>
 
 // A final class
+/**
+ * @brief
+ */
 class Animal final
 {
   private:
     std::string name_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Animal(const std::string &name) : name_(name)
     {
     }
 
+    /**
+     * @brief name getter
+     */
     const std::string &name() const
     {
         return name_;
