91d3c5de3f8dbb42309d1b933404ec5dc544e15e
Emanuele Galli <egalli64@gmail.com>
2024-06-27 16:04:51 +0200

    more info

--- 07_OOP/s5_abstract_final/e1_abstract.cpp
+++ 07_OOP/s5_abstract_final/e1_abstract.cpp
@@ -5,36 +5,62 @@
  */
 #include <iostream>
 
-// An abstract class
+/**
+ * @brief an abstract class
+ */
 class Animal
 {
   private:
     std::string name_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Animal(const std::string &name) : name_(name)
     {
     }
 
+    /**
+     * @brief name getter
+     */
     const std::string &name() const
     {
         return name_;
     }
-    // pure virtual method
+
+    /**
+     * @brief a pure virtual method
+     */
     virtual std::string fullName() const = 0;
+
+    /**
+     * @brief dtor
+     */
+    virtual ~Animal()
+    {
+    }
 };
 
-// derives from an abstract class, to be concrete must override the inherited abstract methods
+/**
+ * @brief derives from an abstract class, to be concrete must override the inherited abstract methods
+ */
 class Dog : public Animal
 {
   private:
     std::string owner_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Dog(const std::string &name, const std::string &owner) : Animal(name), owner_(owner)
     {
     }
 
+    /**
+     * @brief (re)define the pure abstract method from the base class
+     */
     std::string fullName() const override
     {
         return Animal::name() + " owned by " + owner_;
