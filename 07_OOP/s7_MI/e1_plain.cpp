d51c6f6d861330dc557cabb4c8a962a28f2fa576
Emanuele Galli <egalli64@gmail.com>
2024-06-29 17:55:39 +0200

    more info

--- 07_OOP/s7_MI/e1_plain.cpp
+++ 07_OOP/s7_MI/e1_plain.cpp
@@ -5,60 +5,99 @@
  */
 #include <iostream>
 
+/**
+ * @brief a base class
+ */
 class Animal
 {
   private:
-    std::string name_;
+    const std::string name_;
 
   public:
-    Animal(const std::string &name) : name_(name)
+    /**
+     * @brief ctor
+     */
+    Animal(const std::string &name) : name_{name}
     {
         std::cout << "Animal ctor for " << name << '\n';
     }
+
+    /**
+     * @brief dtor
+     */
     virtual ~Animal()
     {
         std::cout << "Animal dtor for " << name_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &name() const
     {
         return name_;
     }
 };
 
+/**
+ * @brief another base class
+ */
 class Player
 {
   private:
-    std::string game_;
+    const std::string game_;
 
   public:
+    /**
+     * @brief ctor
+     */
     Player(const std::string &game) : game_(game)
     {
         std::cout << "Player ctor for " << game << '\n';
     }
+
+    /**
+     * @brief dtor
+     */
     virtual ~Player()
     {
         std::cout << "Player dtor for " << game_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &game() const
     {
         return game_;
     }
 };
 
+/**
+ * @brief Dog IS-An Animal, and IS-A Player, too!
+ */
 class Dog : public Animal, Player
 {
   public:
-    Dog(const std::string &name, const std::string &game) : Animal(name), Player(game)
+    /**
+     * @brief ctor, create the internal objects from left to right
+     */
+    Dog(const std::string &name, const std::string &game) : Animal{name}, Player{game}
     {
         std::cout << "Dog ctor" << '\n';
     }
+
+    /**
+     * @brief dtor, pass the control to the upper level from right to left
+     */
     ~Dog() override
     {
         std::cout << "Dog dtor" << '\n';
     }
 
+    /**
+     * @brief a method that uses both natures of the object
+     */
     void message()
     {
         std::cout << name() << " likes playing " << game() << '\n';
