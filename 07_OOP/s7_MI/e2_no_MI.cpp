f5df82d9c789d4493e06fae6d79b79f5c267e10c
Emanuele Galli <egalli64@gmail.com>
2024-06-29 18:05:53 +0200

    more info

--- 07_OOP/s7_MI/e2_no_MI.cpp
+++ 07_OOP/s7_MI/e2_no_MI.cpp
@@ -5,69 +5,114 @@
  */
 #include <iostream>
 
+/**
+ * @brief a base class
+ */
 class Actor
 {
   private:
-    std::string name_;
+    const std::string name_;
 
   public:
-    Actor(const std::string &name) : name_(name)
+    /**
+     * @brief ctor
+     */
+    Actor(const std::string &name) : name_{name}
     {
         std::cout << "Actor ctor for " << name << '\n';
     }
+
+    /**
+     * @brief dtor
+     */
     virtual ~Actor()
     {
         std::cout << "Actor dtor for " << name_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &name() const
     {
         return name_;
     }
 };
 
+/**
+ * @brief a class in the middle level Animal IS-An Actor
+ */
 class Animal : public Actor
 {
   public:
-    Animal(const std::string &name) : Actor(name)
+    /**
+     * @brief ctor
+     */
+    Animal(const std::string &name) : Actor{name}
     {
         std::cout << "Animal ctor for " << name << '\n';
     }
+
+    /**
+     * @brief dtor
+     */
     ~Animal() override
     {
         std::cout << "Animal dtor" << '\n';
     }
 };
 
+/**
+ * @brief another class in the middle level Player IS-An Actor
+ */
 class Player : public Actor
 {
   private:
-    std::string game_;
+    const std::string game_;
 
   public:
-    Player(const std::string &name, const std::string &game) : Actor(name), game_(game)
+    /**
+     * @brief ctor
+     */
+    Player(const std::string &name, const std::string &game) : Actor{name}, game_{game}
     {
         std::cout << "Player ctor for " << game << '\n';
     }
+
+    /**
+     * @brief dtor
+     */
     ~Player() override
     {
         std::cout << "Player dtor for " << game_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &game() const
     {
         return game_;
     }
 };
 
+/**
+ * @brief a class in the lower lever
+ */
 class Dog : public Player
 {
   public:
-    Dog(const std::string &name, const std::string &game) : Player(name, game)
+    /**
+     * @brief ctor
+     */
+    Dog(const std::string &name, const std::string &game) : Player{name, game}
     {
         std::cout << "Dog ctor\n";
     }
 
+    /**
+     * @brief a Dog Player method
+     */
     void message()
     {
         std::cout << name() << " likes playing " << game() << '\n';
