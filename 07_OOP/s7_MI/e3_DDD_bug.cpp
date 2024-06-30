f55d7adb807bd4c3acf241efccd80858aa8608c3
Emanuele Galli <egalli64@gmail.com>
2024-06-30 20:05:19 +0200

    more info

--- 07_OOP/s7_MI/e3_DDD_bug.cpp
+++ 07_OOP/s7_MI/e3_DDD_bug.cpp
@@ -5,70 +5,115 @@
  */
 #include <iostream>
 
+/**
+ * @brief base class
+ */
 class Actor
 {
   private:
-    std::string name_;
+    const std::string name_;
 
   public:
-    Actor(const std::string &name) : name_(name)
+    /**
+     * @brief ctor
+     */
+    Actor(const std::string &name) : name_{name}
     {
         std::cout << "Actor ctor for " << name << '\n';
     }
-    ~Actor()
+
+    /**
+     * @brief dtor
+     */
+    virtual ~Actor()
     {
         std::cout << "Actor dtor for " << name_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &name() const
     {
         return name_;
     }
 };
 
+/**
+ * @brief intermediate class - Animal IS-An Actor
+ */
 class Animal : public Actor
 {
   public:
+    /**
+     * @brief ctor
+     */
     Animal(const std::string &name) : Actor(name)
     {
         std::cout << "Animal ctor for " << name << '\n';
     }
-    ~Animal()
+
+    /**
+     * @brief dtor
+     */
+    ~Animal() override
     {
         std::cout << "Animal dtor" << '\n';
     }
 };
 
+/**
+ * @brief intermediate class - Player IS-An Actor
+ */
 class Player : public Actor
 {
   private:
-    std::string game_;
+    const std::string game_;
 
   public:
-    Player(const std::string &name, const std::string &game) : Actor(name), game_(game)
+    /**
+     * @brief ctor
+     */
+    Player(const std::string &name, const std::string &game) : Actor{name}, game_{game}
     {
         std::cout << "Player ctor for " << game << '\n';
     }
-    ~Player()
+
+    /**
+     * @brief dtor
+     */
+    ~Player() override
     {
         std::cout << "Player dtor for " << game_ << '\n';
     }
 
+    /**
+     * @brief getter
+     */
     const std::string &game() const
     {
         return game_;
     }
 };
 
+/**
+ * @brief third level class - Dog IS-An Animal and IS-A Player
+ */
 class Dog : public Animal, Player
 {
   public:
-    // name should be passed both to Animal and Player!
-    Dog(const std::string &name, const std::string &game) : Animal(name), Player(name, game)
+    /**
+     * @brief ctor - name should be passed both to Animal and Player!
+     * @bug a Dog is twice an Actor, once as Animal and once as Player ?!
+     */
+    Dog(const std::string &name, const std::string &game) : Animal{name}, Player{name, game}
     {
         std::cout << "Dog ctor" << '\n';
     }
 
+    /**
+     * @brief a method using the upper levels details
+     */
     void message()
     {
         // ambiguous! name as animal or as player?
@@ -82,5 +127,8 @@ class Dog : public Animal, Player
 int main()
 {
     Dog tom{"Tom", "chess"};
+    std::cout << "*** Tom is created, notice the double call to Animal ctor!\n";
     tom.message();
+
+    std::cout << "*** Tom is about to be deleted, notice the double call to Animal dtor!\n";
 }
