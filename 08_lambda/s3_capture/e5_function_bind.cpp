4c762e509d8a089a06f4231b2b69d1fa13e5250a
Emanuele Galli <egalli64@gmail.com>
2024-07-01 10:47:37 +0200

    more info

--- 08_lambda/s3_capture/e5_function_bind.cpp
+++ 08_lambda/s3_capture/e5_function_bind.cpp
@@ -11,18 +11,27 @@
 
 namespace
 {
-template <typename IT> void print(IT first, IT last, const std::string &message = "")
+/**
+ * @brief a utility function
+ */
+template <typename IT> void print(IT begin, IT end, const std::string &message = "")
 {
-    std::cout << message << ": ";
-    std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
+    std::cout << message;
+    std::copy(begin, end, std::ostream_iterator<int>(std::cout, " "));
     std::cout << '\n';
 }
 
+/**
+ * @brief a function meant to be bound to a HOF
+ */
 void increaseValueBy(int &value, int delta)
 {
     value += delta;
 }
 
+/**
+ * @brief a function meant to be bound to a HOF
+ */
 void increaseByValue(int delta, int &value)
 {
     value += delta;
@@ -32,7 +41,7 @@ void increaseByValue(int delta, int &value)
 int main()
 {
     std::vector<int> values{1, 2, 3};
-    print(values.cbegin(), values.cend(), "A few values");
+    print(values.cbegin(), values.cend(), "A few values: ");
 
     std::cout << "Increase by [0 .. 3) using for_each + function (placeholder 1st)\n";
     for (int i = 0; i < 3; ++i)
