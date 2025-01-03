3af3022e82d71578765e60571d64bcf826109864
Emanuele Galli <egalli64@gmail.com>
2025-01-03 22:36:54 +0100

    using a given name from a namespace

---
+++ 04_function/s3_namespace/e3_using_name.cpp
@@ -0,0 +1,16 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * namespace - accessing std
+ */
+#include <iostream>
+
+using std::cout;
+
+/**
+ * @brief using the cout from std namespace
+ */
+int main()
+{
+    cout << "Hello\n";
+}
