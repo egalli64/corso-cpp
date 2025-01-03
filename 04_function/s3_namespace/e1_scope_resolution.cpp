90c0e2a53ae4c043a213ffacfa6c06645a7f8ea7
Emanuele Galli <egalli64@gmail.com>
2025-01-03 22:26:46 +0100

    scope resolution

---
+++ 04_function/s3_namespace/e1_scope_resolution.cpp
@@ -0,0 +1,14 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * namespace - accessing std
+ */
+#include <iostream>
+
+/**
+ * @brief accessing cout from std by scope resolution
+ */
+int main()
+{
+    std::cout << "Hello\n";
+}
