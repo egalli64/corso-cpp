a49555f7777e8e434846b8644be660767ea50a38
Emanuele Galli <egalli64@gmail.com>
2025-01-03 22:34:04 +0100

    using

---
+++ 04_function/s3_namespace/e2_using.cpp
@@ -0,0 +1,16 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * namespace - accessing std
+ */
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief using the std namespace
+ */
+int main()
+{
+    cout << "Hello\n";
+}
