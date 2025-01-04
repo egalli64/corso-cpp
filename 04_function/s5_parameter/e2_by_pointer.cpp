4b543dcd8c6691d57ad84661ef5fbead4a2e2c45
Emanuele Galli <egalli64@gmail.com>
2025-01-04 16:30:46 +0100

    review

--- 04_function/s5_parameter/e2_by_pointer.cpp
+++ 04_function/s5_parameter/e2_by_pointer.cpp
@@ -1,10 +1,13 @@
 /*
  * Corso C++ https://github.com/egalli64/corso-cpp
  *
- * by pointer
+ * parameter - by pointer
  */
 #include <iostream>
 
+/**
+ * @brief a function getting arguments by pointer
+ */
 void swap(int *pLeft, int *pRight)
 {
     std::cout << "Called by pointer: left is " << *pLeft << " and right is " << *pRight << '\n';
