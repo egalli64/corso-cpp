9e0486ed8a06492ec966cb4bd425a7ed4c04666c
Emanuele Galli <egalli64@gmail.com>
2025-01-08 13:15:09 +0100

    review

--- 07_OOP/s8_private_inheritance/e1_private.cpp
+++ 07_OOP/s8_private_inheritance/e1_private.cpp
@@ -1,7 +1,7 @@
 /*
  * Corso C++ https://github.com/egalli64/corso-cpp
  *
- * Protected inheritance
+ * OOP: Private inheritance
  */
 #include <iostream>
 
@@ -26,10 +26,10 @@ class Tail
     }
 };
 
-// Dog has-a Tail, and shares its access to derived classes
-class Dog : protected Tail
+// Dog has-a Tail
+class Dog : private Tail
 {
-  protected:
+  private:
     std::string name_;
 
   public:
@@ -42,34 +42,12 @@ class Dog : protected Tail
         std::cout << name_ << ": bark!\n";
         wag(name_);
     }
-
-    virtual void eat()
-    {
-        std::cout << name_ << ": eat\n";
-    }
-};
-
-class Poodle : public Dog
-{
-  public:
-    Poodle(const std::string &name) : Dog(name, 2)
-    {
-    }
-
-    void eat() override
-    {
-        std::cout << name_ << ": eat\n";
-        wag(name_);
-    }
 };
 
 int main()
 {
     Dog tom{"Tom", 3};
     tom.bark();
-    tom.eat();
-
-    Poodle bob{"Bob"};
-    bob.bark();
-    bob.eat();
+    // Tail methods are not part of the dog interface
+    // tom.wag();
 }
