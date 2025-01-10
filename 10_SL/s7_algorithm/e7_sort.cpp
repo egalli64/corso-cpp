1a98242fa45e8cf4a1edc3a93c3ac2abe3a03d80
Emanuele Galli <egalli64@gmail.com>
2025-01-10 11:17:50 +0100

    algorithm: sort

---
+++ 10_SL/s7_algorithm/e7_sort.cpp
@@ -0,0 +1,28 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * SL algorithm: sort
+ */
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void print(const std::vector<std::string> &items, const std::string &label)
+{
+    std::cout << label << " [ ";
+    for (const auto &item : items)
+    {
+        std::cout << item << ' ';
+    }
+    std::cout << "]\n";
+}
+
+int main()
+{
+    std::vector<std::string> data{"tom", "bob", "kim"};
+    print(data, "A string vector");
+    std::sort(data.begin(), data.end());
+    // std::stable_sort(data.begin(), data.end());
+    print(data, "Sorted");
+}
