f69c1576de18ce5287565ba33e3fdcc8cd7f4fff
Emanuele Galli <egalli64@gmail.com>
2025-01-15 18:22:40 +0100

    split for floating point

---
+++ 02_data_types/s03_fundamentals/e4_floating.cpp
@@ -0,0 +1,12 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * Fundamental types - floating point
+ */
+#include <iostream>
+
+int main()
+{
+    double d = 3.14159'26535;
+    std::cout << "A double: " << d << '\n';
+}
