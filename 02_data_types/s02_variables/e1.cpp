77f633ce97a71be0ebbd9465a0f3c34a2b45f592
Emanuele Galli <egalli64@gmail.com>
2025-01-15 12:35:37 +0100

    review

--- 02_data_types/s02_variables/e1.cpp
+++ 02_data_types/s02_variables/e1.cpp
@@ -16,18 +16,21 @@ int main()
     // MSVC: warnings C6001 and C4700 (/sdl for error)
     // std::cout << "!!! BAD IDEA !!! " << a << '\n';
 
-    a = 5; // a is initialized to 5
+    // a is initialized to 5
+    a = 5;
+
+    // using the variable a in an instruction (to print its value)
     std::cout << a << '\n';
 
     // variable definition preferred to split declaration and initialization
     int b = 7;
     std::cout << b << '\n';
 
-    // assignment, changing value in a variable
-    b = 42;
-    std::cout << b << '\n';
-
     // the compiler deduces the variable type using the value on the right side
     auto c = 21;
+    std::cout << c << '\n';
+
+    // assignment, changing value in a variable
+    c = 42;
     std::cout << c << '\n';
 }
