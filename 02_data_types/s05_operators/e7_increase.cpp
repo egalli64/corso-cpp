60aa73cab6c82076691484f57fac49bfa8bb92e4
Emanuele Galli <egalli64@gmail.com>
2025-01-16 23:33:11 +0100

    split for increase and decrease

---
+++ 02_data_types/s05_operators/e7_increase.cpp
@@ -0,0 +1,18 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * Operators: increase / decrease
+ */
+#include <iostream>
+
+int main()
+{
+    int x = 42;
+    int y = 18;
+
+    std::cout << "x is " << x << '\n';
+    std::cout << "y is " << y << '\n';
+
+    std::cout << "after ++x the value in x is " << ++x << '\n';
+    std::cout << "after --x the value in x is " << --x << '\n';
+}
