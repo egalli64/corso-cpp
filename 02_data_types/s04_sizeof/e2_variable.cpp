5df0c6a538b569bfb175725ce67f5fb243888cf6
Emanuele Galli <egalli64@gmail.com>
2025-01-16 18:38:09 +0100

    clarify

--- 02_data_types/s04_sizeof/e2_variable.cpp
+++ 02_data_types/s04_sizeof/e2_variable.cpp
@@ -9,5 +9,9 @@ int main()
 {
     int a = 42;
     std::cout << sizeof(int) << '\n';
-    std::cout << sizeof(a) << '\n';
+    std::cout << sizeof(a) << "\n\n";
+
+    double d = 4.2;
+    std::cout << sizeof(double) << '\n';
+    std::cout << sizeof(d) << '\n';
 }
