5a190e52eb45fd14f724ee0895249320419c9f23
Emanuele Galli <egalli64@gmail.com>
2025-01-17 12:15:55 +0100

    split example on string

---
+++ 02_data_types/s06_cast/e2_string.cpp
@@ -0,0 +1,20 @@
+/*
+ * Corso C++ https://github.com/egalli64/corso-cpp
+ *
+ * sort of casts between numeric types and string
+ */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s = "42";
+    int i = std::stoi(s);
+    std::cout << "From string to int (pre-incremented): " << ++i << '\n';
+
+    s += ".27";
+    double d{std::stod(s)};
+    std::cout << "From string to double: " << d << '\n';
+
+    std::cout << "From numeric to string, and concat: " << std::to_string(51) + std::to_string(8.931255) << '\n';
+}
