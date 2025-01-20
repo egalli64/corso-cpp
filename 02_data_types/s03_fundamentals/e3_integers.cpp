b8fb13ff1094a4eee16554c91ebe067dd2c23b25
Emanuele Galli <egalli64@gmail.com>
2025-01-20 17:30:15 +0100

    clarify

--- 02_data_types/s03_fundamentals/e3_integers.cpp
+++ 02_data_types/s03_fundamentals/e3_integers.cpp
@@ -8,11 +8,11 @@
 int main()
 {
     // optional digit separator C++14
-    int i = 49'385;
+    int i = 49'385'949;
     std::cout << "An int: " << i << '\n';
 
     //  Hexadecimal notation
-    signed short int ssi = 0xFFFF;
-    unsigned short int usi = 0xFFFF;
+    signed short int ssi = 0xFFFF; // all bits set to 1 -> -1
+    unsigned short int usi = 0xFFFF; // all bits set to 1 -> 65535
     std::cout << "A signed short integer: " << ssi << " and an unsigned short int: " << usi << '\n';
 }
