b4df38d22afc4bc68002913144fbbb3982d42a25
Emanuele Galli <egalli64@gmail.com>
2025-01-20 17:28:13 +0100

    clarify

--- 02_data_types/s02_variables/e1.cpp
+++ 02_data_types/s02_variables/e1.cpp
@@ -14,23 +14,23 @@ int main()
     int a;
     // GCC: when compiled with -Wall flag -> warning: uninitialized
     // MSVC: warnings C6001 and C4700 (/sdl for error)
-    // std::cout << "!!! BAD IDEA !!! " << a << '\n';
+    std::cout << "!!! BAD IDEA !!! Accessing an uninitialized variable: " << a << '\n';
 
     // a is initialized to 5
     a = 5;
 
     // using the variable a in an instruction (to print its value)
-    std::cout << a << '\n';
+    std::cout << "Variable initialized to " << a << '\n';
 
     // variable definition preferred to split declaration and initialization
     int b = 7;
-    std::cout << b << '\n';
+    std::cout << "Variable defined as " << b << '\n';
 
     // the compiler deduces the variable type using the value on the right side
     auto c = 21;
-    std::cout << c << '\n';
+    std::cout << "Variable type deduced by compiler by its initializing value " << c << '\n';
 
     // assignment, changing value in a variable
     c = 42;
-    std::cout << c << '\n';
+    std::cout << "After assignment: " << c << '\n';
 }
