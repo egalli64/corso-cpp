4e2761ac9675b597a8f6cbc78c07c46670043824
Emanuele Galli <egalli64@gmail.com>
2025-01-21 22:11:25 +0100

    clarify

--- 06_OOP/s2_class/e1_struct.cpp
+++ 06_OOP/s2_class/e1_struct.cpp
@@ -16,17 +16,21 @@ struct Point
 
 int main()
 {
+    // aggregate initialization
     Point a{5, 7};
     std::cout << a.x << ' ' << a.y << '\n';
 
+    // accessing the Point object by pointer
     Point *p = &a;
+    p->x += 5;
     std::cout << p->x << ' ' << p->y << '\n';
 
+    // accessing the Point object by reference
     Point &r = a;
     r.x += 5;
     std::cout << r.x << ' ' << r.y << '\n';
 
-    // !!! not initialized !!!
+    // !!! a non-initialized object !!!
     Point b;
     std::cout << b.x << ' ' << b.y << '\n';
 }
