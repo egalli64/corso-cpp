e250a7869f79ae4f975f192b46f8920137e7ab4f
Emanuele Galli <egalli64@gmail.com>
2025-01-23 16:27:37 +0100

    clarify

--- 10_SL/s2_vector/e1_ctor.cpp
+++ 10_SL/s2_vector/e1_ctor.cpp
@@ -19,19 +19,20 @@ template <typename T> void print(const std::vector<T> &values, const std::string
 
 int main()
 {
+    // an empty vector, implicit call to the default ctor
     std::vector<int> empty;
     std::cout << "Size of an empty vector is " << empty.size() << '\n';
 
+    // another empty vector, explicit call to the default ctor
     std::vector<int> anotherEmpty{};
-    std::cout << "Size of another empty vector is " << empty.size() << '\n';
+    std::cout << "Size of another empty vector is " << anotherEmpty.size() << '\n';
 
+    // a vector sized 5, all the element initialized to -1
     // notice: here can't be used the unversal initializer!
     std::vector<int> filled(5, -1);
     print(filled, "A vector filled with a given value");
 
-    // no filling for vector
-    // values.fill(-1);
-
+    // a vector sized 2, with 6 and 4 in it
     // notice the difference between the fill ctor and the initializer list ctor
     std::vector<int> given{6, 4};
     print(given, "A vector with two given values");
