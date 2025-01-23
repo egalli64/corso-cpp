a3962697492a03dacc2f9672de4cd4a013cf54d6
Emanuele Galli <egalli64@gmail.com>
2025-01-23 16:17:49 +0100

    readability

--- 09_memory/s4_shared/e5_copy.cpp
+++ 09_memory/s4_shared/e5_copy.cpp
@@ -13,12 +13,13 @@ int main()
 
     if (sp.unique())
     {
+        // a second shared ptr on the same object
         std::shared_ptr<Thing> sp2{sp};
         std::cout << "A new shared ptr created by copy just increase the count\n";
+        std::cout << "The use count is set to " << sp.use_count() << '\n';
         if (!sp.unique())
         {
-            std::cout << "There are more owners\n";
-            std::cout << "The use count is set to " << sp.use_count() << '\n';
+            std::cout << "There are more owners for the same resource!\n";
         }
     }
     else
@@ -26,10 +27,6 @@ int main()
         std::cout << "Unexpected!\n";
     }
 
-    if (sp.unique())
-    {
-        std::cout << "The shared ptr dtor decrease the use count\n";
-    }
-
+    std::cout << "The second shared ptr went out of scope, use count is set to " << sp.use_count() << '\n';
     std::cout << "The last shared ptr exiting of scope causes the resource destruction\n";
 }
